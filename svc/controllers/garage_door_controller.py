09151367cc073c1a5c1d341c4d645c3286f9b91d
Jon Graf <jongraf1@gmail.com>
2020-03-13 19:22:28 -0500

    refactored garage door controller class to utilize common validation method

--- svc/controllers/garage_door_controller.py
+++ svc/controllers/garage_door_controller.py
@@ -5,13 +5,11 @@ from svc.utilities.user_garage_utils import get_garage_url_by_user
 from svc.utilities import api_utils
 
 
-# TODO: check response from api utils to return custom response
 def get_status(bearer_token, user_id):
     is_jwt_valid(bearer_token)
     base_url = get_garage_url_by_user(user_id)
     status, data = api_utils.get_garage_door_status(bearer_token, base_url)
-    if status > 200:
-        raise BadRequest(description='Garage node returned a failure')
+    __validate_response(status)
     return data
 
 
@@ -19,16 +17,18 @@ def update_state(bearer_token, user_id, request):
     is_jwt_valid(bearer_token)
     base_url = get_garage_url_by_user(user_id)
     status, data = api_utils.update_garage_door_state(bearer_token, base_url, request)
-    if status > 200:
-        raise BadRequest(description='Garage node returned a failure')
+    __validate_response(status)
     return data
 
 
-# TODO: check response from api utils to return custom response
 def toggle_door(bearer_token, user_id):
     is_jwt_valid(bearer_token)
     base_url = get_garage_url_by_user(user_id)
     status = api_utils.toggle_garage_door_state(bearer_token, base_url)
+    __validate_response(status)
+    return status
+
+
+def __validate_response(status):
     if status > 200:
         raise BadRequest(description='Garage node returned a failure')
-    return status
