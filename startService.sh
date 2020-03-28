cc828c4ce5c93f0b7378c50a6d0ef38b953857a7
Jon Graf <jongraf1@gmail.com>
2020-03-28 18:12:38 -0500

    added print statements to service starting script for visual validation

--- startService.sh
+++ startService.sh
@@ -2,16 +2,17 @@
 
 
 HOME_AUTO_SERVICE_FILE=homeAutomation.service
-
+YELLOW='\033[1;33m'
+WHITE='\033[0m'
 
 function cloneServiceFiles {
     if [[ -d "/home/pi/home_automation_api" ]]
     then
-        echo "Directory exists."
+        echo -e "${YELLOW}---------------Service Folder Exists---------------${WHITE}"
         cd /home/pi/home_automation_api
         git pull
     else
-        echo "Directory does not exist."
+        echo -e "${YELLOW}---------------Cloning Service---------------${WHITE}"
         cd /home/pi/
         git https://github.com/jonny561201/home_automation_api.git
     fi
@@ -19,24 +20,29 @@ function cloneServiceFiles {
 }
 
 function installDependencies {
+    echo -e "${YELLOW}---------------Installing Dependencies---------------${WHITE}"
     pip3 install -Ur requirements.txt
 }
 
 function stopService {
+    echo -e "${YELLOW}---------------Stopping Service---------------${WHITE}"
     sudo systemctl stop ${HOME_AUTO_SERVICE_FILE}
 }
 
 function copyServiceFile {
+    echo  -e "${YELLOW}---------------Creating SystemD---------------${WHITE}"
     sudo chmod 644 ${HOME_AUTO_SERVICE_FILE}
     sudo yes | cp ${HOME_AUTO_SERVICE_FILE} /lib/systemd/system/${HOME_AUTO_SERVICE_FILE}
 }
 
 function configureSystemD {
+    echo  -e "${YELLOW}---------------Configuring SystemD---------------${WHITE}"
     sudo systemctl daemon-reload
     sudo systemctl enable ${HOME_AUTO_SERVICE_FILE}
 }
 
 function restartDevice {
+    echo  -e "${YELLOW}---------------Rebooting Device---------------${WHITE}"
     sudo reboot
 }
 
