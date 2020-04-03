acb5d6ac975057ca368ce1881d9de4edb843a99a
Jon Graf <jongraf1@gmail.com>
2020-04-03 18:05:04 -0500

    removed unnecessary comment

--- svc/routes/app_routes.py
+++ svc/routes/app_routes.py
@@ -13,7 +13,6 @@ def health_check():
     return "Success"
 
 
-# TODO: Add multiple roles per user
 @APP_BLUEPRINT.route('/login', methods=['GET'])
 def app_login():
     basic_token = request.headers.get('Authorization')
