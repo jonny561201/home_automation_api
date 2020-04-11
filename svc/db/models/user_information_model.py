1d494ac5301d99a8679b2729a68c0d9eaec047ec
Jon Graf <jongraf1@gmail.com>
2020-04-10 20:21:57 -0500

    added orm mapping for the new tables and incorporated them into the other objectzx

--- svc/db/models/user_information_model.py
+++ svc/db/models/user_information_model.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Boolean, Integer, ForeignKey, DECIMAL, TIMESTAMP, DATE
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import UUID, INET, SMALLINT
 from sqlalchemy.ext import declarative
 from sqlalchemy.orm import relationship
 
@@ -31,6 +31,29 @@ class UserRoles(Base):
     user_id = Column(UUID, ForeignKey(UserInformation.id))
 
     role = relationship('Roles', foreign_keys='UserRoles.role_id')
+    role_devices = relationship("RoleDevices", backref="parent", uselist=False, lazy='joined')
+
+
+class RoleDevices(Base):
+    __tablename__ = 'role_devices'
+
+    id = Column(UUID, nullable=False, primary_key=True)
+    ip_address = Column(INET, nullable=False)
+    max_nodes = Column(SMALLINT, nullable=False)
+    user_role_id = Column(UUID, ForeignKey(UserRoles.id))
+
+    role_device_nodes = relationship("RoleDeviceNodes", backref="parent", lazy='joined')
+
+
+class RoleDeviceNodes(Base):
+    __tablename__ = 'role_device_nodes'
+
+    id = Column(UUID, nullable=False, primary_key=True)
+    node_name = Column(String, nullable=False)
+    node_device = Column(SMALLINT, nullable=True)
+    role_device_id = Column(UUID, ForeignKey(RoleDevices.id))
+
+    role_device = relationship('RoleDevices', foreign_keys='RoleDeviceNodes.role_device_id')
 
 
 class UserPreference(Base):
@@ -54,6 +77,7 @@ class UserCredentials(Base):
     user_id = Column(UUID, ForeignKey(UserInformation.id))
 
     user = relationship('UserInformation', foreign_keys='UserCredentials.user_id')
+    user_roles = relationship("UserRoles", secondaryjoin="and_(UserRoles.user_id == UserInformation.id)", secondary='user_information')
 
 
 class DailySumpPumpLevel(Base):
