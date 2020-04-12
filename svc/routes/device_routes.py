e76e61a3fe1cb635eb530a7f4febe9efa4be6c86
Jon Graf <jongraf1@gmail.com>
2020-04-12 13:27:37 -0500

    grabbing the request body and passing it serialized down to the controller

--- svc/routes/device_routes.py
+++ svc/routes/device_routes.py
@@ -1,7 +1,10 @@
+import json
+
 from flask import request
 from svc.controllers.devices_controller import add_device_to_role
 
 
 def add_device_by_user_id(user_id):
     bearer_token = request.headers.get('Authorization')
-    add_device_to_role(bearer_token, user_id, None)
+    request_data = json.loads(request.data.decode('UTF-8'))
+    add_device_to_role(bearer_token, user_id, request_data)
--- test/unit/routes/test_device_routes.py
+++ test/unit/routes/test_device_routes.py
@@ -1,3 +1,5 @@
+import json
+
 from mock import patch, ANY
 
 from svc.routes.device_routes import add_device_by_user_id
@@ -11,10 +13,18 @@ class TestDeviceRoutes:
 
     def test_add_device_by_user__should_pass_bearer_token_to_controller(self, mock_request, mock_controller):
         mock_request.headers = {'Authorization': self.BEARER_TOKEN}
+        mock_request.data = json.dumps({}).encode('UTF-8')
         add_device_by_user_id(self.USER_ID)
         mock_controller.assert_called_with(self.BEARER_TOKEN, ANY, ANY)
 
     def test_add_device_by_user__should_pass_user_id_to_controller(self, mock_request, mock_controller):
         mock_request.headers = {'Authorization': None}
+        mock_request.data = json.dumps({}).encode('UTF-8')
         add_device_by_user_id(self.USER_ID)
         mock_controller.assert_called_with(ANY, self.USER_ID, ANY)
+
+    def test_add_device_by_user__should_pass_the_decoded_request_body_to_controller(self, mock_request, mock_controller):
+        request_data = {'fakeData': 'Im Not Real'}
+        mock_request.data = json.dumps(request_data).encode('UTF-8')
+        add_device_by_user_id(self.USER_ID)
+        mock_controller.assert_called_with(ANY, ANY, request_data)
