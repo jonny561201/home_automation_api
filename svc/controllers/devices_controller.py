b79889baaa69f7600df97737d6f8fc80713cdccd
Jon Graf <jongraf1@gmail.com>
2020-04-12 13:11:32 -0500

    refactored user id to be passed into devices controller as separate from body

--- svc/controllers/devices_controller.py
+++ svc/controllers/devices_controller.py
@@ -4,10 +4,10 @@ from svc.utilities.jwt_utils import is_jwt_valid
 from svc.db.methods.user_credentials import UserDatabaseManager
 
 
-def add_device_to_role(bearer_token, request_data):
+def add_device_to_role(bearer_token, user_id, request_data):
     is_jwt_valid(bearer_token)
     with UserDatabaseManager() as database:
         try:
-            database.add_new_role_device(request_data['userId'], request_data['roleName'], request_data['ipAddress'])
+            database.add_new_role_device(user_id, request_data['roleName'], request_data['ipAddress'])
         except KeyError:
             raise BadRequest
--- test/unit/controllers/test_devices_controller.py
+++ test/unit/controllers/test_devices_controller.py
@@ -14,26 +14,26 @@ class TestDeviceController:
     IP_ADDRESS = '192.168.0.55'
 
     def test_add_device_to_role__should_validate_jwt(self, mock_jwt, mock_db):
-        request_data = {'userId': None, 'roleName': None, 'ipAddress': None}
-        add_device_to_role(self.BEARER_TOKEN, request_data)
+        request_data = {'roleName': None, 'ipAddress': None}
+        add_device_to_role(self.BEARER_TOKEN, self.USER_ID, request_data)
         mock_jwt.assert_called_with(self.BEARER_TOKEN)
 
     def test_add_device_to_role__should_call_add_new_role_with_user_id(self, mock_jwt, mock_db):
-        request_data = {'userId': self.USER_ID, 'roleName': None, 'ipAddress': None}
-        add_device_to_role(self.BEARER_TOKEN, request_data)
+        request_data = {'roleName': None, 'ipAddress': None}
+        add_device_to_role(self.BEARER_TOKEN, self.USER_ID, request_data)
         mock_db.return_value.__enter__.return_value.add_new_role_device.assert_called_with(self.USER_ID, ANY, ANY)
 
     def test_add_device_to_role__should_call_add_new_role_with_role_name(self, mock_jwt, mock_db):
-        request_data = {'userId': None, 'roleName': self.ROLE_NAME, 'ipAddress': None}
-        add_device_to_role(self.BEARER_TOKEN, request_data)
+        request_data = {'roleName': self.ROLE_NAME, 'ipAddress': None}
+        add_device_to_role(self.BEARER_TOKEN, self.USER_ID, request_data)
         mock_db.return_value.__enter__.return_value.add_new_role_device.assert_called_with(ANY, self.ROLE_NAME, ANY)
 
     def test_add_device_to_role__should_call_add_new_role_with_ip_address(self, mock_jwt, mock_db):
-        request_data = {'userId': None, 'roleName': None, 'ipAddress': self.IP_ADDRESS}
-        add_device_to_role(self.BEARER_TOKEN, request_data)
+        request_data = {'roleName': None, 'ipAddress': self.IP_ADDRESS}
+        add_device_to_role(self.BEARER_TOKEN, self.USER_ID, request_data)
         mock_db.return_value.__enter__.return_value.add_new_role_device.assert_called_with(ANY, ANY, self.IP_ADDRESS)
 
     def test_add_device_to_role__should_raise_bad_request_exception_if_key_missing(self, mock_jwt, mock_db):
         request_data = {}
         with pytest.raises(BadRequest):
-            add_device_to_role(self.BEARER_TOKEN, request_data)
+            add_device_to_role(self.BEARER_TOKEN, self.USER_ID, request_data)
