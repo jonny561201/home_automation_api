6ad99d35aa24d4f5e9a8ebcd67236ce715ee9972
Jon Graf <jongraf1@gmail.com>
2020-04-12 12:58:39 -0500

    device controller will now query the database method to add a new role device

--- svc/controllers/devices_controller.py
+++ svc/controllers/devices_controller.py
@@ -1,5 +1,8 @@
 from svc.utilities.jwt_utils import is_jwt_valid
+from svc.db.methods.user_credentials import UserDatabaseManager
 
 
 def add_device_to_role(bearer_token, request_data):
     is_jwt_valid(bearer_token)
+    with UserDatabaseManager() as database:
+        database.add_new_role_device(request_data['userId'], None, None)
--- test/unit/controllers/test_devices_controller.py
+++ test/unit/controllers/test_devices_controller.py
@@ -1,13 +1,20 @@
-from mock import patch
+from mock import patch, ANY
 
 from svc.controllers.devices_controller import add_device_to_role
 
 
+@patch('svc.controllers.devices_controller.UserDatabaseManager')
 @patch('svc.controllers.devices_controller.is_jwt_valid')
 class TestDeviceController:
     BEARER_TOKEN = '123abcd'
+    USER_ID = '78890abvc'
 
-    def test_add_device_to_role__should_validate_jwt(self, mock_jwt):
-        request_data = {}
+    def test_add_device_to_role__should_validate_jwt(self, mock_jwt, mock_db):
+        request_data = {'userId': None}
         add_device_to_role(self.BEARER_TOKEN, request_data)
         mock_jwt.assert_called_with(self.BEARER_TOKEN)
+
+    def test_add_device_to_role__should_call_add_new_role_with_user_id(self, mock_jwt, mock_db):
+        request_data = {'userId': self.USER_ID}
+        add_device_to_role(self.BEARER_TOKEN, request_data)
+        mock_db.return_value.__enter__.return_value.add_new_role_device.assert_called_with(self.USER_ID, ANY, ANY)
