952f9f712753bc9ee83c7daed8a1d91742fe33ff
Jon Graf <jongraf1@gmail.com>
2020-04-12 13:04:42 -0500

    device controller will pass along the ip address to the database layer

--- test/unit/controllers/test_devices_controller.py
+++ test/unit/controllers/test_devices_controller.py
@@ -9,18 +9,24 @@ class TestDeviceController:
     BEARER_TOKEN = '123abcd'
     USER_ID = '78890abvc'
     ROLE_NAME = 'test_role'
+    IP_ADDRESS = '192.168.0.55'
 
     def test_add_device_to_role__should_validate_jwt(self, mock_jwt, mock_db):
-        request_data = {'userId': None, 'roleName': None}
+        request_data = {'userId': None, 'roleName': None, 'ipAddress': None}
         add_device_to_role(self.BEARER_TOKEN, request_data)
         mock_jwt.assert_called_with(self.BEARER_TOKEN)
 
     def test_add_device_to_role__should_call_add_new_role_with_user_id(self, mock_jwt, mock_db):
-        request_data = {'userId': self.USER_ID, 'roleName': None}
+        request_data = {'userId': self.USER_ID, 'roleName': None, 'ipAddress': None}
         add_device_to_role(self.BEARER_TOKEN, request_data)
         mock_db.return_value.__enter__.return_value.add_new_role_device.assert_called_with(self.USER_ID, ANY, ANY)
 
     def test_add_device_to_role__should_call_add_new_role_with_role_name(self, mock_jwt, mock_db):
-        request_data = {'userId': None, 'roleName': self.ROLE_NAME}
+        request_data = {'userId': None, 'roleName': self.ROLE_NAME, 'ipAddress': None}
         add_device_to_role(self.BEARER_TOKEN, request_data)
         mock_db.return_value.__enter__.return_value.add_new_role_device.assert_called_with(ANY, self.ROLE_NAME, ANY)
+
+    def test_add_device_to_role__should_call_add_new_role_with_ip_address(self, mock_jwt, mock_db):
+        request_data = {'userId': None, 'roleName': None, 'ipAddress': self.IP_ADDRESS}
+        add_device_to_role(self.BEARER_TOKEN, request_data)
+        mock_db.return_value.__enter__.return_value.add_new_role_device.assert_called_with(ANY, ANY, self.IP_ADDRESS)
