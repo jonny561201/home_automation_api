3e7281634cc4130bd7daa33f63524aca1b4c9931
Jon Graf <jongraf1@gmail.com>
2020-05-23 15:53:55 -0500

    updated garage utilities to make call to database should we not be in dev mode

--- svc/utilities/user_garage_utils.py
+++ svc/utilities/user_garage_utils.py
@@ -1,4 +1,13 @@
+from svc.constants.settings_state import Settings
+from svc.db.methods.user_credentials import UserDatabaseManager
+
+
 def get_garage_url_by_user(user_id):
+    settings = Settings.get_instance().get_settings()
+    if settings['Development']:
+        return 'http://localhost:5001'
+    with UserDatabaseManager() as database:
+        database.get_user_garage_ip(user_id)
+
     # TODO: get ip address from the database!!!  if in testing mode use local host
-    return 'http://localhost:5001'
     # return 'http://192.168.1.175:5001'
--- test/unit/utilities/test_user_garage_utils.py
+++ test/unit/utilities/test_user_garage_utils.py
@@ -1,15 +1,22 @@
+from mock import patch
+
 from svc.constants.settings_state import Settings
 from svc.utilities.user_garage_utils import get_garage_url_by_user
 
 
+@patch('svc.utilities.user_garage_utils.Settings')
+@patch('svc.utilities.user_garage_utils.UserDatabaseManager')
 class TestGarageUtils:
     USER_ID = 'heyImAUserId'
 
-    def setup_method(self):
-        self.SETTINGS = Settings.get_instance().get_settings()
-
-    def test_get_garage_url_by_user__should_return_localhost_url_when_dev_mode(self):
-        self.SETTINGS['Development'] = True
+    def test_get_garage_url_by_user__should_return_localhost_url_when_dev_mode(self, mock_db, mock_settings):
+        mock_settings.get_instance.return_value.get_settings.return_value = {'Development': True}
         actual = get_garage_url_by_user(self.USER_ID)
 
         assert actual == 'http://localhost:5001'
+
+    def test_get_garage_url_by_user__should_call_database_when_not_dev_mode(self, mock_db, mock_settings):
+        mock_settings.get_instance.return_value.get_settings.return_value = {'Development': False}
+        get_garage_url_by_user(self.USER_ID)
+
+        mock_db.return_value.__enter__.return_value.get_user_garage_ip.assert_called_with(self.USER_ID)
