8a030a5278d66d4b9f6f399f1efe9d55baf8381f
Jon Graf <jongraf1@gmail.com>
2020-10-28 22:12:59 -0500

    updated light alarm service orchestrator to make database call to get light group id preference

--- svc/services/light_service.py
+++ svc/services/light_service.py
@@ -1,6 +1,7 @@
 from svc.constants.home_automation import Automation
 from svc.constants.lights_state import LightState
 from svc.constants.settings_state import Settings
+from svc.db.methods.user_credentials import UserDatabaseManager
 from svc.utilities.api_utils import get_light_api_key
 from svc.utilities.event_utils import create_thread
 from svc.utilities.light_utils import run_light_program
@@ -11,5 +12,6 @@ def start_light_alarm():
     if state.ALARM_THREAD is None:
         settings = Settings.get_instance()
         api_key = get_light_api_key(settings.light_api_user, settings.light_api_password)
-        group_id = ''
+        with UserDatabaseManager() as database:
+            group_id = database.get_preferences_by_user('some user id')['alarm_light_group']
         create_thread(state, lambda: run_light_program(api_key, group_id), Automation.TIME.FIVE_SECONDS)
--- test/unit/services/test_light_service.py
+++ test/unit/services/test_light_service.py
@@ -6,6 +6,7 @@ from svc.constants.settings_state import Settings
 from svc.services.light_service import start_light_alarm
 
 
+@patch('svc.services.light_service.UserDatabaseManager')
 @patch('svc.services.light_service.get_light_api_key')
 @patch('svc.services.light_service.create_thread')
 class TestLightService:
@@ -14,22 +15,28 @@ class TestLightService:
 
     def setup_method(self):
         self.LIGHTS = LightState.get_instance()
+        self.LIGHTS.ALARM_THREAD = None
         self.SETTINGS = Settings.get_instance()
         self.SETTINGS.dev_mode = True
         self.SETTINGS.settings = {"LightApiPass": self.PASSWORD, "LightApiUser": self.USER_NAME}
 
-    def test_start_light_alarm__should_create_thread(self, mock_thread, mock_api):
+    def test_start_light_alarm__should_create_thread(self, mock_thread, mock_api, mock_db):
         start_light_alarm()
 
         mock_thread.assert_called_with(self.LIGHTS, ANY, Automation.TIME.FIVE_SECONDS)
 
-    def test_start_light_alarm__should_make_call_to_get_api_key(self, mock_thread, mock_api):
+    def test_start_light_alarm__should_make_call_to_get_api_key(self, mock_thread, mock_api, mock_db):
         start_light_alarm()
 
         mock_api.assert_called_with(self.USER_NAME, self.PASSWORD)
 
-    def test_start_light_alarm__should_not_make_api_call_when_thread_already_created(self, mock_thread, mock_api):
+    def test_start_light_alarm__should_not_make_api_call_when_thread_already_created(self, mock_thread, mock_api, mock_db):
         self.LIGHTS.ALARM_THREAD = {}
         start_light_alarm()
 
         mock_api.assert_not_called()
+
+    def test_start_light_alarm__should_make_call_to_get_alarm_light_group(self, mock_thread, mock_api, mock_db):
+        start_light_alarm()
+
+        mock_db.return_value.__enter__.return_value.get_preferences_by_user.assert_called()
