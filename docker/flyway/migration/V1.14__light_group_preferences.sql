009d3583725d5f6155f9d5bbb970c88343c9ef66
Jon Graf <jongraf1@gmail.com>
2020-10-29 15:46:34 -0500

    updated sql script to include an alarm time along with the days of the week

--- docker/flyway/migration/V1.14__light_group_preferences.sql
+++ docker/flyway/migration/V1.14__light_group_preferences.sql
@@ -1,1 +1,3 @@
 ALTER TABLE user_preferences ADD COLUMN alarm_light_group VARCHAR(3);
+ALTER TABLE user_preferences ADD COLUMN alarm_time TIME;
+ALTER TABLE user_preferences ADD COLUMN alarm_days VARCHAR(21);
