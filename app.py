a596a82a976fbd7b9b2158ca6e808566b1c6c4a4
Jon Graf <jongraf1@gmail.com>
2020-11-01 14:17:51 -0600

    added light alarm creation to beginning of app...at least it works locally

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from svc.manager import create_app
+from svc.services.light_service import create_start_light_alarm
 
+create_start_light_alarm()
 main_app = create_app(__name__)
 main_app.run(host='0.0.0.0', port=5000)
