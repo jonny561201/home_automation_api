1e09ee613b265a3e1d1d1e0cca5d371227460c32
Jon Graf <jongraf1@gmail.com>
2020-11-02 17:14:27 -0600

    added column to user preferences database for light name

--- docker/flyway/migration/V1.14__light_group_preferences.sql
+++ docker/flyway/migration/V1.14__light_group_preferences.sql
@@ -1,3 +1,4 @@
 ALTER TABLE user_preferences ADD COLUMN alarm_light_group VARCHAR(3);
 ALTER TABLE user_preferences ADD COLUMN alarm_time TIME;
 ALTER TABLE user_preferences ADD COLUMN alarm_days VARCHAR(21);
+ALTER TABLE user_preferences ADD COLUMN alarm_light_name VARCHAR(255);
