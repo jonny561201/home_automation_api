c27029fbda0a8885a545429eea6d3d82f736b8b6
Jon Graf <jongraf1@gmail.com>
2020-12-07 22:41:22 -0600

    updated the create light alarm to now use the new add replace light alarm function and no longer return alarm as stored on light state

--- svc/services/light_service.py
+++ svc/services/light_service.py
@@ -1,7 +1,7 @@
 from werkzeug.exceptions import FailedDependency
 
 from svc.constants.home_automation import Automation
-from svc.constants.lights_state import LightAlarm
+from svc.constants.lights_state import LightState
 from svc.constants.settings_state import Settings
 from svc.db.methods.user_credentials import UserDatabaseManager
 from svc.utilities.api_utils import get_light_api_key
@@ -9,21 +9,19 @@ from svc.utilities.event_utils import create_thread
 from svc.utilities.light_utils import run_light_program
 
 
-# TODO: how should I get the devices default user id....would be easier of the database was cloud based...
-# loop over this guy to create multiple light alarms
+# TODO: how should I get the devices default user id....would be easier if the database was cloud based...
 def create_start_light_alarm():
     settings = Settings.get_instance()
     with UserDatabaseManager() as database:
         preference = database.get_preferences_by_user(settings.user_id).get('light_alarm')
     if preference['alarm_time'] is not None and preference['alarm_days'] is not None:
-        return create_alarm(settings.light_api_user, settings.light_api_password, preference)
+        __create_alarm(settings.light_api_user, settings.light_api_password, preference)
 
 
-def create_alarm(light_user, light_pass, preference):
+def __create_alarm(light_user, light_pass, preference):
     try:
         api_key = get_light_api_key(light_user, light_pass)
-        alarm = LightAlarm(preference['alarm_time'], preference['alarm_days'])
+        alarm = LightState.get_instance().add_replace_light_alarm(preference['alarm_light_group'], preference['alarm_time'], preference['alarm_days'])
         create_thread(alarm, lambda: run_light_program(alarm, api_key, preference['alarm_light_group']), Automation.TIME.TEN_SECONDS)
-        return alarm
     except FailedDependency:
-        return None
+        pass
--- test/unit/services/test_light_service.py
+++ test/unit/services/test_light_service.py
@@ -9,7 +9,7 @@ from svc.constants.settings_state import Settings
 from svc.services.light_service import create_start_light_alarm
 
 
-@patch('svc.services.light_service.LightAlarm')
+@patch('svc.services.light_service.LightState')
 @patch('svc.services.light_service.UserDatabaseManager')
 @patch('svc.services.light_service.get_light_api_key')
 @patch('svc.services.light_service.create_thread')
@@ -17,61 +17,57 @@ class TestLightService:
     USER_NAME = 'test user'
     PASSWORD = 'test pass'
     USER_ID = 'def098'
+    GROUP_ID = 'abc123'
 
     def setup_method(self):
-        self.ALARM = LightAlarm(datetime.time(), 'MonTue')
+        self.ALARM = LightAlarm(self.GROUP_ID, datetime.time(), 'MonTue')
         self.SETTINGS = Settings.get_instance()
         self.SETTINGS.dev_mode = True
         self.SETTINGS.settings = {"LightApiPass": self.PASSWORD, "LightApiUser": self.USER_NAME, 'UserId': self.USER_ID}
 
-    def test_create_start_light_alarm__should_create_thread(self, mock_thread, mock_api, mock_db, mock_alarm):
-        mock_alarm.return_value = self.ALARM
+    def test_create_start_light_alarm__should_create_thread(self, mock_thread, mock_api, mock_db, mock_light):
+        mock_light.get_instance.return_value.add_replace_light_alarm.return_value = self.ALARM
         create_start_light_alarm()
 
         mock_thread.assert_called_with(self.ALARM, ANY, Automation.TIME.TEN_SECONDS)
 
-    def test_create_start_light_alarm__should_make_call_to_get_api_key(self, mock_thread, mock_api, mock_db, mock_alarm):
+    def test_create_start_light_alarm__should_make_call_to_get_api_key(self, mock_thread, mock_api, mock_db, mock_light):
         create_start_light_alarm()
 
         mock_api.assert_called_with(self.USER_NAME, self.PASSWORD)
 
-    def test_create_start_light_alarm__should_make_call_to_get_alarm_light_group(self, mock_thread, mock_api, mock_db, mock_alarm):
+    def test_create_start_light_alarm__should_make_call_to_get_alarm_light_group(self, mock_thread, mock_api, mock_db, mock_light):
         create_start_light_alarm()
 
         mock_db.return_value.__enter__.return_value.get_preferences_by_user.assert_called()
 
-    def test_create_start_light_alarm__should_make_call_to_get_alarm_light_group_using_user_id_from_settings(self, mock_thread, mock_api, mock_db, mock_alarm):
+    def test_create_start_light_alarm__should_make_call_to_get_alarm_light_group_using_user_id_from_settings(self, mock_thread, mock_api, mock_db, mock_light):
         create_start_light_alarm()
 
         mock_db.return_value.__enter__.return_value.get_preferences_by_user.assert_called_with(self.USER_ID)
 
-    def test_create_start_light_alarm__should_create_light_state_object(self, mock_thread, mock_api, mock_db, mock_alarm):
+    def test_create_start_light_alarm__should_create_light_state_object(self, mock_thread, mock_api, mock_db, mock_light):
         time = datetime.time()
         days = 'MonTueWed'
-        mock_db.return_value.__enter__.return_value.get_preferences_by_user.return_value = {'light_alarm': {'alarm_time': time, 'alarm_days': days}}
+        group_id = 12
+        mock_db.return_value.__enter__.return_value.get_preferences_by_user.return_value = {'light_alarm': {'alarm_time': time, 'alarm_days': days, 'alarm_light_group': group_id}}
         create_start_light_alarm()
 
-        mock_alarm.assert_called_with(time, days)
+        mock_light.get_instance.return_value.add_replace_light_alarm.assert_called_with(group_id, time, days)
 
-    def test_create_start_light_alarm__should_return_alarm_to_allow_later_cancellation(self, mock_thread, mock_api, mock_db, mock_alarm):
-        mock_alarm.return_value = self.ALARM
-        actual = create_start_light_alarm()
-
-        assert actual == self.ALARM
-
-    def test_create_start_light_alarm__should_not_create_thread_when_alarm_time_is_none(self, mock_thread, mock_api, mock_db, mock_alarm):
+    def test_create_start_light_alarm__should_not_create_thread_when_alarm_time_is_none(self, mock_thread, mock_api, mock_db, mock_light):
         mock_db.return_value.__enter__.return_value.get_preferences_by_user.return_value = {'city': 'Des Moines', 'light_alarm': {'alarm_time': None, 'alarm_days': 'montue'}}
         create_start_light_alarm()
 
         mock_thread.assert_not_called()
 
-    def test_create_start_light_alarm__should_not_create_thread_when_alarm_is_none(self, mock_thread, mock_api, mock_db, mock_alarm):
+    def test_create_start_light_alarm__should_not_create_thread_when_alarm_is_none(self, mock_thread, mock_api, mock_db, mock_light):
         mock_db.return_value.__enter__.return_value.get_preferences_by_user.return_value = {'city': 'Des Moines', 'light_alarm': {'alarm_time': datetime.time(), 'alarm_days': None}}
         create_start_light_alarm()
 
         mock_thread.assert_not_called()
 
-    def test_create_start_light_alarm__should_return_none_when_api_key_raises_bad_dependency(self, mock_thread, mock_api, mock_db, mock_alarm):
+    def test_create_start_light_alarm__should_return_none_when_api_key_raises_bad_dependency(self, mock_thread, mock_api, mock_db, mock_light):
         mock_api.side_effect = FailedDependency()
         actual = create_start_light_alarm()
 
