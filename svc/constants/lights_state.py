ee9795c6d72dc6ced589765d43912024015b0bf6
Jon Graf <jongraf1@gmail.com>
2020-12-07 19:47:29 -0600

    updated the replace light alarm function to create a new light alarm to replace the existing one after cancellation

--- svc/constants/lights_state.py
+++ svc/constants/lights_state.py
@@ -1,8 +1,7 @@
 import datetime
 
 
-# TODO: have light state replace existing light alarm by group id
-# TODO: have light state cancel alarm by group id
+# TODO: have light state cache the api key and get it for you too
 class LightState:
     __instance = None
     API_KEY = None
@@ -19,9 +18,11 @@ class LightState:
         alarm.LIGHT_GROUP_ID = light_group_id
         self.LIGHT_ALARMS.append(alarm)
 
-    def replace_light_alarm(self, light_group_id):
-        alarm = next(x for x in self.LIGHT_ALARMS if x.LIGHT_GROUP_ID == light_group_id)
-        alarm.STOP_EVENT.set()
+    def replace_light_alarm(self, light_group_id, alarm_time, alarm_days):
+        index = next(i for i, x in enumerate(self.LIGHT_ALARMS) if x.LIGHT_GROUP_ID == light_group_id)
+        existing_alarm = self.LIGHT_ALARMS.pop(index)
+        existing_alarm.STOP_EVENT.set()
+        self.LIGHT_ALARMS.append(LightAlarm(alarm_time, alarm_days))
 
     @staticmethod
     def get_instance():
--- test/unit/constants/test_light_state.py
+++ test/unit/constants/test_light_state.py
@@ -8,23 +8,35 @@ from svc.constants.lights_state import LightState, LightAlarm
 
 class TestLightState:
     GROUP_ID = 4
+    TIME = datetime.time.fromisoformat('00:00:01')
+    DAYS = 'MonTueWed'
 
     def setup_method(self):
         self.STATE = LightState.get_instance()
         self.STATE.LIGHT_ALARMS = []
 
     def test_add_light_alarm__should_create_new_light_alarm(self):
-        self.STATE.add_light_alarm(datetime.time.fromisoformat('00:00:01'), 'Mon', self.GROUP_ID)
+        self.STATE.add_light_alarm(self.TIME, self.DAYS, self.GROUP_ID)
         assert len(self.STATE.LIGHT_ALARMS) == 1
         assert self.STATE.LIGHT_ALARMS[0].LIGHT_GROUP_ID == self.GROUP_ID
 
     def test_replace_light_alarm__should_cancel_existing_alarm(self):
         test_event = mock.create_autospec(Event)
-        existing_alarm = LightAlarm(datetime.time.fromisoformat('00:00:01'), 'Mon')
+        existing_alarm = LightAlarm(self.TIME, self.DAYS)
         existing_alarm.STOP_EVENT = test_event
         existing_alarm.LIGHT_GROUP_ID = self.GROUP_ID
         self.STATE.LIGHT_ALARMS.append(existing_alarm)
-        self.STATE.replace_light_alarm(self.GROUP_ID)
+        self.STATE.replace_light_alarm(self.GROUP_ID, self.TIME, self.DAYS)
 
         assert len(self.STATE.LIGHT_ALARMS) == 1
         test_event.set.assert_called()
+
+    def test_replace_light_alarm__should_create_a_new_alarm(self):
+        existing_alarm = LightAlarm(self.TIME, self.DAYS)
+        existing_alarm.STOP_EVENT = mock.create_autospec(Event)
+        existing_alarm.LIGHT_GROUP_ID = self.GROUP_ID
+        self.STATE.LIGHT_ALARMS.append(existing_alarm)
+        self.STATE.replace_light_alarm(self.GROUP_ID, self.TIME, self.DAYS)
+
+        assert len(self.STATE.LIGHT_ALARMS) == 1
+        assert self.STATE.LIGHT_ALARMS[0] != existing_alarm
