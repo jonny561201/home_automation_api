7e63b44d18aa4e5443f7cd836ac0c6857973eb34
Jon Graf <jongraf1@gmail.com>
2020-12-18 22:32:13 -0600

    updated flask app to utilize https...need to manually get cert to get it working on server

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ from svc.services.light_service import create_start_light_alarm
 
 create_start_light_alarm()
 main_app = create_app(__name__)
-main_app.run(host='0.0.0.0', port=5000)
+context = ('certificate.crt', 'private.key')
+main_app.run(host='0.0.0.0', port=5000, ssl_context=context)
