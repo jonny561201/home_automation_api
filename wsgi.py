0f55de3d696f80bd0251980236eb3f46bf0ae938
Jon Graf <jongraf1@gmail.com>
2020-12-20 14:31:58 -0600

    walking the refactor back a step to move the create light alarm to manager

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from svc.manager import app
+from svc.manager import create_app
 
-if __name__ == 'main':
-    app.run()
+app = create_app(__name__)
+app.run(host='0.0.0.0', port=5000, ssl_context=('certificate.crt', 'private.key'))
