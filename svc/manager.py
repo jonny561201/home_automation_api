094dd69358f50632bfa9c412fdf59981389acf23
Jon Graf <jongraf1@gmail.com>
2020-12-20 14:21:52 -0600

    attempting change to allow wsgi to spin up server

--- svc/manager.py
+++ svc/manager.py
@@ -8,18 +8,17 @@ from svc.routes.garage_door_routes import GARAGE_BLUEPRINT
 from svc.routes.light_routes import LIGHT_BLUEPRINT
 from svc.routes.sump_routes import SUMP_BLUEPRINT
 from svc.routes.thermostat_routes import THERMOSTAT_BLUEPRINT
+from svc.services.light_service import create_start_light_alarm
 
 
-def create_app(app_name):
-    if app_name == '__main__':
-        app = Flask(app_name)
-        CORS(app)
-        app.register_blueprint(APP_BLUEPRINT)
-        app.register_blueprint(ACCOUNT_BLUEPRINT)
-        app.register_blueprint(SUMP_BLUEPRINT)
-        app.register_blueprint(THERMOSTAT_BLUEPRINT)
-        app.register_blueprint(GARAGE_BLUEPRINT)
-        app.register_blueprint(LIGHT_BLUEPRINT)
-        app.register_blueprint(DEVICES_BLUEPRINT)
+create_start_light_alarm()
+app = Flask(__name__)
+CORS(app)
+app.register_blueprint(APP_BLUEPRINT)
+app.register_blueprint(ACCOUNT_BLUEPRINT)
+app.register_blueprint(SUMP_BLUEPRINT)
+app.register_blueprint(THERMOSTAT_BLUEPRINT)
+app.register_blueprint(GARAGE_BLUEPRINT)
+app.register_blueprint(LIGHT_BLUEPRINT)
+app.register_blueprint(DEVICES_BLUEPRINT)
 
-        return app
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,4 @@
-from svc.manager import create_app
-from svc.services.light_service import create_start_light_alarm
+from svc.manager import app
 
-create_start_light_alarm()
-main_app = create_app(__name__)
-context = ('certificate.crt', 'private.key')
-main_app.run(host='0.0.0.0', port=5000, ssl_context=context)
+if __name__ == 'main':
+    app.run()
