3fa3bd356cd7919e96a120c6d6a8e59afd9e8705
Jon Graf <jongraf1@gmail.com>
2021-01-15 18:11:30 -0600

    fixed missed unit test from prior cleanup

--- test/unit/controllers/test_app_controller.py
+++ test/unit/controllers/test_app_controller.py
@@ -55,14 +55,12 @@ class TestLoginController:
         mock_db.return_value.__enter__.return_value.get_preferences_by_user.assert_called_with(self.USER_ID)
 
     def test_get_user_preferences__should_return_preferences_response(self, mock_jwt, mock_db):
-        alarm_time = datetime.datetime.now().time()
-        prefs = {'unit': 'imperial', 'city': 'Des Moines', 'light_alarm': {'alarm_time': alarm_time}}
-        expected_prefs = {'unit': 'imperial', 'city': 'Des Moines', 'light_alarm': {'alarm_time': str(alarm_time)}}
+        prefs = {'unit': 'imperial', 'city': 'Des Moines'}
         mock_db.return_value.__enter__.return_value.get_preferences_by_user.return_value = prefs
 
         actual = get_user_preferences(self.BASIC_AUTH_TOKEN, self.USER_ID)
 
-        assert actual == expected_prefs
+        assert actual == prefs
 
     def test_save_user_preferences__should_validate_bearer_token(self, mock_jwt, mock_db):
         bearer_token = 'fakeBearerToken'
