0dac869db79645021bed3eeeb1a15fd7368cb9d5
Jon Graf <jongraf1@gmail.com>
2021-01-15 20:15:08 -0600

    removed light alarm state from the home automation rest api into its own service

--- svc/constants/lights_state.py
+++ svc/constants/lights_state.py
@@ -1,10 +1,5 @@
-import datetime
-
-from svc.constants.home_automation import Automation
 from svc.constants.settings_state import Settings
 from svc.utilities.api_utils import get_light_api_key
-from svc.utilities.event_utils import create_thread
-from svc.utilities.light_utils import run_light_program
 
 
 class LightState:
@@ -18,18 +13,6 @@ class LightState:
         else:
             LightState.__instance = self
 
-    def add_replace_light_alarm(self, light_group_id, alarm_time, alarm_days):
-        if light_group_id is None or alarm_time is None:
-            return None
-        index = next((i for i, x in enumerate(self.LIGHT_ALARMS) if x.LIGHT_GROUP_ID == light_group_id), None)
-        if index is not None:
-            existing_alarm = self.LIGHT_ALARMS.pop(index)
-            existing_alarm.STOP_EVENT.set()
-        alarm = LightAlarm(light_group_id, alarm_time, alarm_days)
-        create_thread(alarm, lambda: run_light_program(alarm, self.get_light_api_key(), light_group_id), Automation.TIME.TEN_SECONDS)
-        self.LIGHT_ALARMS.append(alarm)
-        return alarm
-
     def get_light_api_key(self):
         if self.API_KEY is None:
             settings = Settings.get_instance()
@@ -41,19 +24,3 @@ class LightState:
         if LightState.__instance is None:
             LightState.__instance = LightState()
         return LightState.__instance
-
-
-class LightAlarm:
-    ALARM_COUNTER = 0
-    ACTIVE_THREAD = None
-    STOP_EVENT = None
-    ALARM_START_TIME = None
-    ALARM_STOP_TIME = None
-    ALARM_DAYS = None
-    LIGHT_GROUP_ID = None
-
-    def __init__(self, light_group_id, alarm_time, alarm_days):
-        self.LIGHT_GROUP_ID = light_group_id
-        self.ALARM_DAYS = alarm_days
-        self.ALARM_START_TIME = (datetime.datetime.combine(datetime.date.today(), alarm_time) + datetime.timedelta(minutes=-10)).time()
-        self.ALARM_STOP_TIME = (datetime.datetime.combine(datetime.date.today(), alarm_time) + datetime.timedelta(minutes=+10)).time()
