13961de592ba60817b59a23686f97ef0c012c3ec
Jon Graf <jongraf1@gmail.com>
2021-01-24 17:47:02 -0600

    turning off the app controller bearer token verification for getting user tasks for the light api service

--- svc/controllers/app_controller.py
+++ svc/controllers/app_controller.py
@@ -25,7 +25,7 @@ def save_user_preferences(bearer_token, user_id, request_data):
 
 
 def get_user_tasks(bearer_token, user_id):
-    jwt_utils.is_jwt_valid(bearer_token)
+    # jwt_utils.is_jwt_valid(bearer_token)
     with UserDatabaseManager() as database:
         return database.get_schedule_tasks_by_user(user_id)
 
