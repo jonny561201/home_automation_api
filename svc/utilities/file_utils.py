e4bce9fc8451a26162b2eccf92a92b4245272054
Jon Graf <jongraf1@gmail.com>
2021-02-27 19:17:33 -0600

    renamed file name variable to be more specifically for temp

--- svc/utilities/file_utils.py
+++ svc/utilities/file_utils.py
@@ -4,7 +4,7 @@ from svc.constants.settings_state import Settings
 
 
 def write_status_to_file(door_number, time):
-    file_name = Settings.get_instance().file_name
+    file_name = Settings.get_instance().temp_file_name
     try:
         with open(file_name, 'r', encoding='utf-8') as file:
             content = json.load(file)
