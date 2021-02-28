cba966d2510926b792b72489ebf7008a72cfff00
Jon Graf <jongraf1@gmail.com>
2021-02-27 19:26:46 -0600

    updated the file utility method to more correctly write desired temp and current mode to file

--- svc/utilities/file_utils.py
+++ svc/utilities/file_utils.py
@@ -3,16 +3,17 @@ import json
 from svc.constants.settings_state import Settings
 
 
-def write_status_to_file(door_number, time):
+def write_desired_temp_to_file(metric_temp, mode):
     file_name = Settings.get_instance().temp_file_name
     try:
         with open(file_name, 'r', encoding='utf-8') as file:
             content = json.load(file)
-            content[door_number] = f'{time:%Y-%m-%d %H:%M:%S%z}'
+            content['desiredTemp'] = metric_temp
+            content['mode'] = mode
 
         with open(file_name, "w") as file:
             json.dump(content, file)
     except FileNotFoundError:
-        content = {door_number: f'{time:%Y-%m-%d %H:%M:%S%z}'}
+        content = {'desiredTemp': metric_temp, 'mode': mode}
         with open(file_name, "w+") as file:
             json.dump(content, file)
