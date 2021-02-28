5e46f7f86e09106a15b7c2d5311e1bf94d105bc1
Jon Graf <jongraf1@gmail.com>
2021-02-27 19:12:17 -0600

    added method to write temperature date to a file

---
+++ svc/utilities/file_utils.py
@@ -0,0 +1,18 @@
+import json
+
+from svc.constants.settings_state import Settings
+
+
+def write_status_to_file(door_number, time):
+    file_name = Settings.get_instance().file_name
+    try:
+        with open(file_name, 'r', encoding='utf-8') as file:
+            content = json.load(file)
+            content[door_number] = f'{time:%Y-%m-%d %H:%M:%S%z}'
+
+        with open(file_name, "w") as file:
+            json.dump(content, file)
+    except FileNotFoundError:
+        content = {door_number: f'{time:%Y-%m-%d %H:%M:%S%z}'}
+        with open(file_name, "w+") as file:
+            json.dump(content, file)
