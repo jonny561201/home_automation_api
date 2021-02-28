a7317e1ab43189d4858d021fe03d12faba2096e6
Jon Graf <jongraf1@gmail.com>
2021-02-28 16:06:32 -0600

    updated to actually read the internal temperature sensor file

--- svc/utilities/gpio_utils.py
+++ svc/utilities/gpio_utils.py
@@ -1,3 +1,6 @@
 def read_temperature_file():
-    return ['72 01 4b 46 7f ff 0e 10 57 : crc=57 YES',
-            '72 01 4b 46 7f ff 0e 10 57 t=23125']
+    file_name = "/sys/bus/w1/devices/28-000006dfa76c/w1_slave"
+    with open(file_name, 'r', encoding='utf-8') as file:
+        return file.read().split("\n")
+        return ['72 01 4b 46 7f ff 0e 10 57 : crc=57 YES',
+                '72 01 4b 46 7f ff 0e 10 57 t=23125']
