1658429ca1ae00faa4b32336b80d4c41b752a866
Jon Graf <jongraf1@gmail.com>
2021-03-02 17:35:12 -0600

    bipassing reading the real temperature file until I know how the file shows up and is named

--- svc/utilities/gpio_utils.py
+++ svc/utilities/gpio_utils.py
@@ -1,6 +1,6 @@
 def read_temperature_file():
-    file_name = "/sys/bus/w1/devices/28-000006dfa76c/w1_slave"
+    return ['72 01 4b 46 7f ff 0e 10 57 : crc=57 YES',
+            '72 01 4b 46 7f ff 0e 10 57 t=23125']
     with open(file_name, 'r', encoding='utf-8') as file:
         return file.read().split("\n")
-        return ['72 01 4b 46 7f ff 0e 10 57 : crc=57 YES',
-                '72 01 4b 46 7f ff 0e 10 57 t=23125']
+    file_name = "/sys/bus/w1/devices/28-000006dfa76c/w1_slave"
