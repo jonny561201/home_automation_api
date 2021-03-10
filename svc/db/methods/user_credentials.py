e0ab25d8deaa0d5f197fce0c661ac38b78c351e4
Jon Graf <jongraf1@gmail.com>
2021-03-09 20:28:36 -0600

    update scheduled task by user will use the original hvac stop date if none is provided

--- svc/db/methods/user_credentials.py
+++ svc/db/methods/user_credentials.py
@@ -84,7 +84,7 @@ class UserDatabase:
         old_task.alarm_light_group = task['alarmLightGroup'] if task.get('alarmLightGroup') else old_task.alarm_light_group
         old_task.enabled = task['enabled'] if task.get('enabled') is not None else old_task.enabled
         old_task.hvac_start = time.fromisoformat(task['hvacStart']) if task.get('hvacStart') else old_task.hvac_start
-        old_task.hvac_stop = time.fromisoformat(task['hvacStop'])
+        old_task.hvac_stop = time.fromisoformat(task['hvacStop']) if task.get('hvacStop') else old_task.hvac_stop
 
         if old_task.task_type.activity_name != task.get('taskType'):
             old_task.task_type = self.session.query(ScheduledTaskTypes).filter_by(activity_name=task.get('taskType')).first()
--- test/unit/db/methods/test_user_credentials.py
+++ test/unit/db/methods/test_user_credentials.py
@@ -701,6 +701,15 @@ class TestUserDatabase:
 
         assert existing_task.hvac_stop == time.fromisoformat(alarm_time)
 
+    def test_update_schedule_task_by_user_id__should_use_the_original_hvac_stop_if_none(self):
+        task = {'taskId': 'asdfasd', 'alarmGroupName': 'bedroom', 'alarmDays': 'Mon'}
+        hvac_stop = time()
+        existing_task = ScheduleTasks(hvac_stop=hvac_stop, task_type=ScheduledTaskTypes())
+        self.SESSION.query.return_value.filter_by.return_value.first.return_value = existing_task
+        self.DATABASE.update_schedule_task_by_user_id(self.USER_ID, task)
+
+        assert existing_task.hvac_stop == hvac_stop
+
     def test_update_schedule_task_by_user_id__should_use_the_original_hvac_start_if_none(self):
         task = {'taskId': 'asdfasd', 'alarmGroupName': 'bedroom', 'alarmDays': 'Mon'}
         hvac_start = time()
