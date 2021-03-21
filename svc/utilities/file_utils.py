45a0bd206fff24347e497301fdfc208d26db3d1d
Jon Graf <jongraf1@gmail.com>
2021-03-21 13:36:10 -0500

    added is auto mode flag to the desired temp file

--- svc/utilities/file_utils.py
+++ svc/utilities/file_utils.py
@@ -4,18 +4,19 @@ from svc.constants.home_automation import Automation
 from svc.constants.settings_state import Settings
 
 
-def write_desired_temp_to_file(metric_temp, mode):
+def write_desired_temp_to_file(metric_temp, mode, isAuto=False):
     file_name = Settings.get_instance().temp_file_name
     try:
         with open(file_name, 'r', encoding='utf-8') as file:
             content = json.load(file)
             content['desiredTemp'] = metric_temp
             content['mode'] = mode
+            content['isAuto'] = isAuto
 
         with open(file_name, "w") as file:
             json.dump(content, file)
     except FileNotFoundError:
-        content = {'desiredTemp': metric_temp, 'mode': mode}
+        content = {'desiredTemp': metric_temp, 'mode': mode, 'isAuto': isAuto}
         with open(file_name, "w+") as file:
             json.dump(content, file)
 
@@ -26,7 +27,7 @@ def get_desired_temp():
         with open(file_name, 'r', encoding='utf-8') as file:
             return json.load(file)
     except (FileNotFoundError, TypeError):
-        content = {'desiredTemp': 21.1111, 'mode': Automation.HVAC.MODE.TURN_OFF}
+        content = {'desiredTemp': 21.1111, 'mode': Automation.HVAC.MODE.TURN_OFF, 'isAuto': False}
         with open(file_name, "w+") as file:
             json.dump(content, file)
         return content
