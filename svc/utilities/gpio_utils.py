a7486dd8206b9f7b3ed93e6318f450e96ac34c29
Jon Graf <jongraf1@gmail.com>
2021-05-01 22:26:08 -0500

    updated the reading of the temperature file to generically locate the sensor file and return zero temp if failure

--- svc/utilities/gpio_utils.py
+++ svc/utilities/gpio_utils.py
@@ -1,6 +1,14 @@
+import glob
+import os
+
+
 def read_temperature_file():
-    return ['72 01 4b 46 7f ff 0e 10 57 : crc=57 YES',
-            '72 01 4b 46 7f ff 0e 10 57 t=23125']
-    with open(file_name, 'r', encoding='utf-8') as file:
-        return file.read().split("\n")
-    file_name = "/sys/bus/w1/devices/28-000006dfa76c/w1_slave"
+    base_dir = '/sys/bus/w1/devices/'
+    file_name = "w1_slave"
+    try:
+        device_folder = glob.glob(base_dir + '28-*')[0]
+        with open(os.path.join(device_folder, file_name), 'r', encoding='utf-8') as file:
+            return file.read().split("\n")
+    except Exception:
+        return ['72 01 4b 46 7f ff 0e 10 57 : crc=57 YES',
+                '72 01 4b 46 7f ff 0e 10 57 t=4078224']
