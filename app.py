9cc71027482cbeaba8fbbc837a9f826d5715de87
Jon Graf <jongraf1@gmail.com>
2021-05-03 17:32:28 -0500

    added gpio configuration of thermostat pin on startup of service

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+import RPi.GPIO as GPIO
 from svc.manager import app
+
+THERMO_PIN = 11
+
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(THERMO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 if __name__ == '__main__':
     app.run()
