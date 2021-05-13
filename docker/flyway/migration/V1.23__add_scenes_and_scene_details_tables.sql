9f06108ef7dd7fcb6835e6610089929ccddaf176
Jon Graf <jongraf1@gmail.com>
2021-05-12 20:37:54 -0500

    added foreign key attachment to user information to have access to user id

--- docker/flyway/migration/V1.23__add_scenes_and_scene_details_tables.sql
+++ docker/flyway/migration/V1.23__add_scenes_and_scene_details_tables.sql
@@ -1,11 +1,12 @@
 create table scenes (
 	ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-	name VARCHAR(30) NOT NULL
+	name VARCHAR(30) NOT NULL,
+    user_id UUID REFERENCES user_information(ID) NOT NULL
 );
 
 create table scene_details (
 	ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    scene_id UUID REFERENCES scene(ID) NOT NULL
+    scene_id UUID REFERENCES scenes(ID) NOT NULL,
 	light_group VARCHAR(3),
 	light_group_name VARCHAR(255),
 	light_brightness SMALLINT
