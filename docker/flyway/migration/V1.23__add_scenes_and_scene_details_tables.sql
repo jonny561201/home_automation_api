1c4fab095d453523b1db3549f6fc0a6cbb8a9653
Jon Graf <jongraf1@gmail.com>
2021-05-12 20:19:11 -0500

    had the foreign key alignment backwards...oops

--- docker/flyway/migration/V1.23__add_scenes_and_scene_details_tables.sql
+++ docker/flyway/migration/V1.23__add_scenes_and_scene_details_tables.sql
@@ -1,12 +1,12 @@
+create table scenes (
+	ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+	name VARCHAR(30) NOT NULL
+);
+
 create table scene_details (
 	ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    scene_id UUID REFERENCES scene(ID) NOT NULL
 	light_group VARCHAR(3),
 	light_group_name VARCHAR(255),
 	light_brightness SMALLINT
-);
-
-create table scenes (
-	ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-	name VARCHAR(30),
-	detail_id UUID REFERENCES scene_details(ID) NOT NULL
 );
