1349494dec8490dc0b46e4be6a7978e85dff8e82
Jon Graf <jongraf1@gmail.com>
2021-05-13 21:08:20 -0500

    get all scene routes by user will now return serialized scenes in response

--- svc/routes/scene_routes.py
+++ svc/routes/scene_routes.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import request, Response
 
 from svc.controllers.scene_controller import get_created_scenes
@@ -8,5 +10,5 @@ DEFAULT_HEADERS = {'Content-Type': 'text/json'}
 
 def get_scenes_by_user(user_id):
     bearer_token = request.headers.get('Authorization')
-    get_created_scenes(bearer_token, user_id)
-    return Response(status=200, headers=DEFAULT_HEADERS)
+    scenes = get_created_scenes(bearer_token, user_id)
+    return Response(json.dumps(scenes), status=200, headers=DEFAULT_HEADERS)
