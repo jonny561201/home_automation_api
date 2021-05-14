0e5028aad0c837f6cb0f2d5fcfb5aeeba22693c8
Jon Graf <jongraf1@gmail.com>
2021-05-14 17:07:40 -0500

    created delete scene route that gets bearer token and passes it down to the controller

--- svc/routes/scene_routes.py
+++ svc/routes/scene_routes.py
@@ -2,7 +2,7 @@ import json
 
 from flask import request, Response, Blueprint
 
-from svc.controllers.scene_controller import get_created_scenes
+from svc.controllers import scene_controller
 
 
 SCENE_BLUEPRINT = Blueprint('scene_routes', __name__, url_prefix='/scenes')
@@ -12,5 +12,10 @@ DEFAULT_HEADERS = {'Content-Type': 'text/json'}
 @SCENE_BLUEPRINT.route('/userId/<user_id>', methods=['GET'])
 def get_scenes_by_user(user_id):
     bearer_token = request.headers.get('Authorization')
-    scenes = get_created_scenes(bearer_token, user_id)
+    scenes = scene_controller.get_created_scenes(bearer_token, user_id)
     return Response(json.dumps(scenes), status=200, headers=DEFAULT_HEADERS)
+
+
+def delete_scene_by_user(user_id, scene_id):
+    bearer_token = request.headers['Authorization']
+    scene_controller.delete_created_scene(bearer_token, None, None)
--- test/unit/routes/test_scene_routes.py
+++ test/unit/routes/test_scene_routes.py
@@ -3,55 +3,63 @@ import uuid
 
 from mock import patch, ANY
 
-from svc.routes.scene_routes import get_scenes_by_user
+from svc.routes.scene_routes import get_scenes_by_user, delete_scene_by_user
 
 
 @patch('svc.routes.scene_routes.request')
-@patch('svc.routes.scene_routes.get_created_scenes')
+@patch('svc.routes.scene_routes.scene_controller')
 class TestSceneRoutes:
     USER_ID = str(uuid.uuid4())
+    SCENE_ID = str(uuid.uuid4())
     BEARER_TOKEN = 'im a bearer token'
     RESPONSE = {'response': 'Im a fake response'}
 
     def test_get_scenes_by_user__should_call_controller_with_bearer_token(self, mock_controller, mock_request):
-        mock_controller.return_value = self.RESPONSE
+        mock_controller.get_created_scenes.return_value = self.RESPONSE
         mock_request.headers = {'Authorization': self.BEARER_TOKEN}
         get_scenes_by_user(self.USER_ID)
 
-        mock_controller.assert_called_with(self.BEARER_TOKEN, ANY)
+        mock_controller.get_created_scenes.assert_called_with(self.BEARER_TOKEN, ANY)
 
     def test_get_scenes_by_user__should_call_controller_with_user_id(self, mock_controller, mock_request):
-        mock_controller.return_value = self.RESPONSE
+        mock_controller.get_created_scenes.return_value = self.RESPONSE
         mock_request.headers = {'Authorization': self.BEARER_TOKEN}
         get_scenes_by_user(self.USER_ID)
 
-        mock_controller.assert_called_with(ANY, self.USER_ID)
+        mock_controller.get_created_scenes.assert_called_with(ANY, self.USER_ID)
 
     def test_get_scenes_by_user__should_call_controller_when_no_auth_header(self, mock_controller, mock_request):
-        mock_controller.return_value = self.RESPONSE
+        mock_controller.get_created_scenes.return_value = self.RESPONSE
         mock_request.headers = {}
         get_scenes_by_user(self.USER_ID)
 
-        mock_controller.assert_called_with(None, self.USER_ID)
+        mock_controller.get_created_scenes.assert_called_with(None, self.USER_ID)
 
     def test_get_scenes_by_user__should_return_success_status_code(self, mock_controller, mock_request):
-        mock_controller.return_value = self.RESPONSE
+        mock_controller.get_created_scenes.return_value = self.RESPONSE
         mock_request.headers = {'Authorization': self.BEARER_TOKEN}
         actual = get_scenes_by_user(self.USER_ID)
 
         assert actual.status_code == 200
 
     def test_get_scenes_by_user__should_return_content_type(self, mock_controller, mock_request):
-        mock_controller.return_value = self.RESPONSE
+        mock_controller.get_created_scenes.return_value = self.RESPONSE
         mock_request.headers = {'Authorization': self.BEARER_TOKEN}
         actual = get_scenes_by_user(self.USER_ID)
 
         assert actual.content_type == 'text/json'
 
     def test_get_scenes_by_user__should_return_response_from_controller(self, mock_controller, mock_request):
-        mock_controller.return_value = self.RESPONSE
+        mock_controller.get_created_scenes.return_value = self.RESPONSE
         mock_request.headers = {'Authorization': self.BEARER_TOKEN}
         actual = get_scenes_by_user(self.USER_ID)
 
         assert json.loads(actual.data) == self.RESPONSE
+
+    def test_delete_scene_by_user__should_call_controller_with_bearer_token(self, mock_controller, mock_request):
+        mock_controller.delete_created_scene.return_value = self.RESPONSE
+        mock_request.headers = {'Authorization': self.BEARER_TOKEN}
+        delete_scene_by_user(self.USER_ID, self.SCENE_ID)
+
+        mock_controller.delete_created_scene.assert_called_with(self.BEARER_TOKEN, ANY, ANY)
 
