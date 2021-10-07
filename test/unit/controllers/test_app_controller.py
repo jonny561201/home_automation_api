102778ba8bbac26dd313ad7cf2e6bf91f6adc933
Jon Graf <jgraf@leantechniques.com>
2021-10-06 20:25:24 -0500

    resolved unresolved merge conflict

--- test/unit/controllers/test_app_controller.py
+++ test/unit/controllers/test_app_controller.py
@@ -219,25 +219,3 @@ class TestLoginController:
         actual = update_user_task(self.BEARER_TOKEN, self.USER_ID, request_data)
 
         assert actual == response
-
-    def test_refresh_bearer_token__should_call_get_roles_by_user_with_user_id(self, mock_jwt, mock_db):
-        old_refresh = str(uuid.uuid4())
-        refresh_bearer_token(self.USER_ID, old_refresh)
-
-        mock_db.return_value.__enter__.return_value.get_roles_by_user.assert_called_with(self.USER_ID)
-
-    def test_refresh_bearer_token__should_call_to_get_a_new_refresh_token(self, mock_jwt, mock_db):
-        old_refresh = str(uuid.uuid4())
-        refresh_bearer_token(self.USER_ID, old_refresh)
-
-        mock_db.return_value.__enter__.return_value.generate_new_refresh_token.assert_called_with(old_refresh)
-
-    def test_refresh_bearer_token__should_call_to_generate_new_bearer_token(self, mock_jwt, mock_db):
-        old_refresh = str(uuid.uuid4())
-        new_refresh = str(uuid.uuid4())
-        user_info = {'user_id': self.USER_ID}
-        mock_db.return_value.__enter__.return_value.get_roles_by_user.return_value = user_info
-        mock_db.return_value.__enter__.return_value.generate_new_refresh_token.return_value = new_refresh
-        refresh_bearer_token(self.USER_ID, old_refresh)
-
-        mock_jwt.create_jwt_token.assert_called_with(user_info, new_refresh)
