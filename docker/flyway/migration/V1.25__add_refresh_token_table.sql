c562911ad7aa43ee462b6ef7f7aa2782d7a6766a
Jon Graf <jgraf@leantechniques.com>
2021-10-10 17:56:41 -0500

    adding the user id to the refresh token and foreign key constraint

--- docker/flyway/migration/V1.25__add_refresh_token_table.sql
+++ docker/flyway/migration/V1.25__add_refresh_token_table.sql
@@ -1,5 +1,6 @@
 create table refresh_token (
     ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES user_information(ID) NOT NULL,
     refresh UUID NOT NULL,
     count SMALLINT NOT NULL DEFAULT 0,
     expire_time TIMESTAMP WITH TIME ZONE NOT NULL
