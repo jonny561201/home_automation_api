07ee0dadc309eb9c085b57555a8f8bb752632ad3
Jon Graf <jgraf@leantechniques.com>
2021-11-06 18:09:59 -0500

    decided it would be easier to include the garage door name as well in preferences

--- docker/flyway/migration/V1.26__add_garage_preference_to_table.sql
+++ docker/flyway/migration/V1.26__add_garage_preference_to_table.sql
@@ -1,1 +1,2 @@
-ALTER TABLE user_preferences ADD COLUMN garage_door smallint;
+ALTER TABLE user_preferences ADD COLUMN garage_id smallint;
+ALTER TABLE user_preferences ADD COLUMN garage_door VARCHAR(100);
