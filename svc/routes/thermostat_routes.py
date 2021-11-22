7bf2f3c127535c96cadef6aae6cc56e69d261df4
Jon Graf <jgraf@leantechniques.com>
2021-11-22 12:50:43 -0600

    added endpoint method to return only the forecast data

--- svc/routes/thermostat_routes.py
+++ svc/routes/thermostat_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, Response
 
 from svc.constants.home_automation import DEFAULT_HEADERS
-from svc.controllers.thermostat_controller import get_user_temp, set_user_temperature
+from svc.controllers import thermostat_controller
 
 THERMOSTAT_BLUEPRINT = Blueprint('thermostat_blueprint', __name__, url_prefix='/thermostat')
 
@@ -9,12 +9,15 @@ THERMOSTAT_BLUEPRINT = Blueprint('thermostat_blueprint', __name__, url_prefix='/
 @THERMOSTAT_BLUEPRINT.route('/temperature/<user_id>', methods=['GET'])
 def get_temperature(user_id):
     bearer_token = request.headers.get('Authorization')
-    return get_user_temp(user_id, bearer_token)
+    return thermostat_controller.get_user_temp(user_id, bearer_token)
 
 
 @THERMOSTAT_BLUEPRINT.route('/temperature/<user_id>', methods=['POST'])
 def set_temperature(user_id):
     bearer_token = request.headers.get('Authorization')
-    set_user_temperature(request.data, bearer_token)
+    thermostat_controller.set_user_temperature(request.data, bearer_token)
     return Response(status=200, headers=DEFAULT_HEADERS)
 
+
+def get_forecast_data(user_id):
+    thermostat_controller.get_user_forecast(user_id, None)
--- test/unit/routes/test_thermostat_routes.py
+++ test/unit/routes/test_thermostat_routes.py
@@ -1,52 +1,56 @@
 import jwt
 from mock import patch, ANY
 
-from svc.routes.thermostat_routes import get_temperature, set_temperature
+from svc.routes.thermostat_routes import get_temperature, set_temperature, get_forecast_data
 
 
-@patch('svc.routes.thermostat_routes.set_user_temperature')
 @patch('svc.routes.thermostat_routes.request')
-@patch('svc.routes.thermostat_routes.get_user_temp')
+@patch('svc.routes.thermostat_routes.thermostat_controller')
 class TestThermostatRoutes:
     JWT_TOKEN = jwt.encode({}, 'JWT_SECRET', algorithm='HS256').decode('UTF-8')
     BEARER_TOKEN = "Bearer " + JWT_TOKEN
     AUTH_HEADER = {"Authorization": BEARER_TOKEN}
     USER_ID = 'test'
 
-    def test_get_temperature__should_call_thermostat_controller(self, mock_controller, mock_request, mock_set):
+    def test_get_temperature__should_call_thermostat_controller(self, mock_controller, mock_request):
         get_temperature(self.USER_ID)
 
-        mock_controller.assert_called()
+        mock_controller.get_user_temp.assert_called()
 
-    def test_get_temperature__should_call_thermostat_controller_with_correct_parameters(self, mock_controller, mock_request, mock_set):
+    def test_get_temperature__should_call_thermostat_controller_with_correct_parameters(self, mock_controller, mock_request):
         mock_request.headers = self.AUTH_HEADER
         get_temperature(self.USER_ID)
 
-        mock_controller.assert_called_with(self.USER_ID, self.BEARER_TOKEN)
+        mock_controller.get_user_temp.assert_called_with(self.USER_ID, self.BEARER_TOKEN)
 
-    def test_get_temperature__should_return_response_from_controller(self, mock_controller, mock_request, mock_set):
+    def test_get_temperature__should_return_response_from_controller(self, mock_controller, mock_request):
         expected_temp = {'currentTemp': 34.12}
-        mock_controller.return_value = expected_temp
+        mock_controller.get_user_temp.return_value = expected_temp
 
         actual = get_temperature(self.USER_ID)
 
         assert actual == expected_temp
 
-    def test_set_temperature__should_call_thermostat_controller(self, mock_controller, mock_request, mock_set):
+    def test_set_temperature__should_call_thermostat_controller(self, mock_controller, mock_request):
         set_temperature(self.USER_ID)
 
-        mock_set.assert_called()
+        mock_controller.set_user_temperature.assert_called()
 
-    def test_set_temperature__should_call_thermostat_controller_with_bearer_token(self, mock_controller, mock_request, mock_set):
+    def test_set_temperature__should_call_thermostat_controller_with_bearer_token(self, mock_controller, mock_request):
         mock_request.headers = self.AUTH_HEADER
         set_temperature(self.USER_ID)
 
-        mock_set.assert_called_with(ANY, self.BEARER_TOKEN)
+        mock_controller.set_user_temperature.assert_called_with(ANY, self.BEARER_TOKEN)
 
-    def test_set_temperature__should_call_thermostat_controller_with_request_body(self, mock_controller, mock_request, mock_set):
+    def test_set_temperature__should_call_thermostat_controller_with_request_body(self, mock_controller, mock_request):
         request = {'desiredTemp': 34.1}
         mock_request.data = request
 
         set_temperature(self.USER_ID)
 
-        mock_set.assert_called_with(request, ANY)
+        mock_controller.set_user_temperature.assert_called_with(request, ANY)
+
+    def test_get_forecast_data__should_call_thermostat_controller_with_user_id(self, mock_controller, mock_request):
+        mock_request.headers = self.AUTH_HEADER
+        get_forecast_data(self.USER_ID)
+        mock_controller.get_user_forecast.assert_called_with(self.USER_ID, ANY)
