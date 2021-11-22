034600eb753c98e24b591582abf607aa0c24a2d2
Jon Graf <jgraf@leantechniques.com>
2021-11-22 15:35:24 -0600

    added success test method for the new endpoint

--- test/integration/test_thermostat_routes_integration.py
+++ test/integration/test_thermostat_routes_integration.py
@@ -27,8 +27,8 @@ class TestThermostatRoutesIntegration:
     def setup_method(self):
         Settings.get_instance().dev_mode = False
         self.USER_ID = uuid.uuid4()
-        self.USER = UserInformation(id=self.USER_ID.hex, first_name='Jon', last_name='Test')
-        self.PREFERENCE = UserPreference(user_id=self.USER_ID.hex, city='London', is_fahrenheit=False, is_imperial=False)
+        self.USER = UserInformation(id=str(self.USER_ID), first_name='Jon', last_name='Test')
+        self.PREFERENCE = UserPreference(user_id=str(self.USER_ID), city='London', is_fahrenheit=False, is_imperial=False)
         flask_app = app
         self.TEST_CLIENT = flask_app.test_client()
         os.environ.update({'JWT_SECRET': self.JWT_SECRET, 'SQL_USERNAME': self.DB_USER,
@@ -67,7 +67,7 @@ class TestThermostatRoutesIntegration:
         bearer_token = jwt.encode({}, self.JWT_SECRET, algorithm='HS256')
         headers = {'Authorization': bearer_token}
 
-        url = 'thermostat/temperature/' + self.USER_ID.hex
+        url = 'thermostat/temperature/' + str(self.USER_ID)
         actual = self.TEST_CLIENT.get(url, headers=headers)
 
         assert actual.status_code == 200
@@ -92,8 +92,31 @@ class TestThermostatRoutesIntegration:
         assert actual.status_code == 200
 
     def test_get_forecast_data__should_return_unauthorized_error_when_invalid_user(self):
-        url = 'thermostat/forecast/' + '3843040'
+        url = 'thermostat/forecast/3843040'
         actual = self.TEST_CLIENT.post(url)
 
         assert actual.status_code == 401
+
+    @patch('svc.utilities.api_utils.requests')
+    def test_get_forecast_data__should_return_successfully(self, mock_request):
+        bearer_token = jwt.encode({}, self.JWT_SECRET, algorithm='HS256')
+        headers = {'Authorization': bearer_token}
+        url = f'thermostat/forecast/{str(self.USER_ID)}'
+
+        response_one = Response()
+        response_two = Response()
+        response_one.status_code = 200
+        response_two.status_code = 200
+        min_temp = 22.0
+        max_temp = 27.3
+        response_one._content = json.dumps({'daily': [{'temp': {'min': min_temp, 'max': max_temp}}]}).encode()
+        response_two._content = json.dumps({'coord': {'lat': 23.232, 'lon': -93.232}}).encode()
+        mock_request.get.side_effect = [response_two, response_one]
+
+        actual = self.TEST_CLIENT.post(url, headers=headers)
+        json_actual = json.loads(actual.data)
+
+        assert actual.status_code == 200
+        assert json_actual['minTemp'] == min_temp
+        assert json_actual['maxTemp'] == max_temp
 
