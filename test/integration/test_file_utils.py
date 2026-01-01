2e30bf8471f4b6467ee52466e0fb8bf9507710a1
Jon Graf <jgraf@vermeer.com>
2026-01-01 13:12:12 -0600

    fixed the test util tests for settings updates

--- test/integration/test_file_utils.py
+++ test/integration/test_file_utils.py
@@ -6,13 +6,15 @@ from svc.utilities.file_utils import write_desired_temp_to_file
 
 
 class TestFileUtils:
+    FILE_NAME = 'test.json'
+
     def setup_method(self):
-        Settings.get_instance(True, {'TempFileName': 'test.json'})
+        settings = Settings.get_instance()
+        settings._settings = {'TempFileName': self.FILE_NAME}
 
     def test_write_desired_temp_to_file__should_default_is_auto_to_false(self):
-        file_name = Settings.get_instance().temp_file_name
         write_desired_temp_to_file(12.2, Automation.HVAC.MODE.HEATING)
-        with open(file_name) as file:
+        with open(self.FILE_NAME) as file:
             content = json.load(file)
             assert content['isAuto'] is False
 
