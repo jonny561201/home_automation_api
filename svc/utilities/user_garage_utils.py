00b5eb4ebeafa1b85ab6e7ddf1c8f42c97617e80
Jon Graf <jgraf@vermeer.com>
2025-12-31 20:32:38 -0600

    updated the garage utils to now use the db for the ip address of the garage door opener

--- svc/utilities/user_garage_utils.py
+++ svc/utilities/user_garage_utils.py
@@ -1,11 +1,7 @@
-from svc.constants.settings_state import Settings
 from svc.db.methods.user_credentials import UserDatabaseManager
 
 
 def get_garage_url_by_user(user_id):
-    settings = Settings.get_instance()
-    if settings.dev_mode:
-        return 'http://localhost:5001'
     with UserDatabaseManager() as database:
         ip = database.get_user_garage_ip(user_id)
-        return 'http://%s' % ip
+        return f'http://{ip}'
--- test/unit/utilities/test_user_garage_utils.py
+++ test/unit/utilities/test_user_garage_utils.py
@@ -5,29 +5,19 @@ from svc.utilities.user_garage_utils import get_garage_url_by_user
 
 
 @patch('svc.utilities.user_garage_utils.UserDatabaseManager')
-class TestGarageUtils:
-    USER_ID = 'heyImAUserId'
+def test_get_garage_url_by_user__should_call_database_when_settings_cannot_be_found(mock_db):
+    user_id = 'heyImAUserId'
+    Settings.get_instance(True, {})
+    get_garage_url_by_user(user_id)
 
-    def setup_method(self):
-        self.SETTINGS = Settings.get_instance()
-        self.SETTINGS.dev_mode = True
+    mock_db.return_value.__enter__.return_value.get_user_garage_ip.assert_called_with(user_id)
 
-    def test_get_garage_url_by_user__should_return_localhost_url_when_dev_mode(self, mock_db):
-        actual = get_garage_url_by_user(self.USER_ID)
+@patch('svc.utilities.user_garage_utils.UserDatabaseManager')
+def test_get_garage_url_by_user__should_return_url_with_ip_from_database(mock_db):
+    user_id = 'heyImAUserId'
+    Settings.get_instance(True, {})
+    database_response = '1.1.1.1'
+    mock_db.return_value.__enter__.return_value.get_user_garage_ip.return_value = database_response
+    actual = get_garage_url_by_user(user_id)
 
-        assert actual == 'http://localhost:5001'
-
-    def test_get_garage_url_by_user__should_call_database_when_settings_cannot_be_found(self, mock_db):
-        self.SETTINGS.dev_mode = False
-        get_garage_url_by_user(self.USER_ID)
-
-        mock_db.return_value.__enter__.return_value.get_user_garage_ip.assert_called_with(self.USER_ID)
-
-    def test_get_garage_url_by_user__should_return_url_with_ip_from_database(self, mock_db):
-        database_response = '1.1.1.1'
-        self.SETTINGS.dev_mode = False
-        mock_db.return_value.__enter__.return_value.get_user_garage_ip.return_value = database_response
-        actual = get_garage_url_by_user(self.USER_ID)
-
-        assert actual == f'http://{database_response}'
-
+    assert actual == f'http://{database_response}'
