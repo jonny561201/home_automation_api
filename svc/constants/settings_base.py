1dd3b589128da5d3b5f5c1a8190f3e6a596b452a
Jon Graf <jgraf@vermeer.com>
2026-01-01 13:08:00 -0600

    broke apart the settings object to have the singleton behavior isolated...might make DI testing easier down the road

---
+++ svc/constants/settings_base.py
@@ -0,0 +1,82 @@
+import os
+
+
+class SettingsBase:
+    _settings = None
+
+    def __init__(self, settings):
+        self._settings = settings
+        self.Queue = Queue(self._settings)
+        self.Database = Database(self._settings)
+
+    @property
+    def email_app_id(self):
+        return self._settings.get('DevEmailAppId') if self._settings is not None else os.environ.get('EMAIL_APP_ID')
+
+    @property
+    def weather_app_id(self):
+        return self._settings.get('DevWeatherAppId') if self._settings is not None else os.environ.get('WEATHER_APP_ID')
+
+    @property
+    def jwt_secret(self):
+        return self._settings.get('DevJwtSecret') if self._settings is not None else os.environ.get('JWT_SECRET')
+
+    @property
+    def light_api_key(self):
+        return self._settings.get('lightApiKey') if self._settings is not None else os.environ.get('LIGHT_API_KEY')
+
+    @property
+    def user_id(self):
+        return self._settings.get('UserId') if self._settings is not None else os.environ.get('USER_ID')
+
+    @property
+    def temp_file_name(self):
+        return self._settings.get('TempFileName') if self._settings is not None else os.environ.get('TEMP_FILE_NAME')
+
+
+class Database:
+
+    def __init__(self, settings):
+        self._settings = settings.get('Database') if settings is not None else None
+
+    @property
+    def user(self):
+        return self._settings.get('User') if self._settings is not None else os.environ.get('SQL_USERNAME')
+
+    @property
+    def password(self):
+        return self._settings.get('Pass') if self._settings is not None else os.environ.get('SQL_PASSWORD')
+
+    @property
+    def name(self):
+        return self._settings.get('Name') if self._settings is not None else os.environ.get('SQL_DBNAME')
+
+    @property
+    def port(self):
+        return self._settings.get('Port') if self._settings is not None else os.environ.get('SQL_PORT')
+
+
+class Queue:
+
+    def __init__(self, settings):
+        self._settings = settings.get('Queue') if settings is not None else None
+
+    @property
+    def user_name(self):
+        return self._settings.get('User') if self._settings is not None else os.environ.get('QUEUE_USER_NAME')
+
+    @property
+    def password(self):
+        return self._settings.get('Password') if self._settings is not None else os.environ.get('QUEUE_PASSWORD')
+
+    @property
+    def host(self):
+        return self._settings.get('Host') if self._settings is not None else os.environ.get('QUEUE_HOST')
+
+    @property
+    def port(self):
+        return self._settings.get('Port') if self._settings is not None else os.environ.get('QUEUE_PORT')
+
+    @property
+    def vhost(self):
+        return self._settings.get('VHost') if self._settings is not None else os.environ.get('QUEUE_VHOST')
