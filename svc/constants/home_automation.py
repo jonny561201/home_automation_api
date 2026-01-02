c75e8c3f82ea0afafbb737c3282f679e3b24b0cf
Jon Graf <jgraf@vermeer.com>
2026-01-02 16:20:01 -0600

    added some common mime types that might be helpful

--- svc/constants/home_automation.py
+++ svc/constants/home_automation.py
@@ -1,5 +1,10 @@
 class Mime:
     JSON = 'application/json'
+    HTML = 'text/html'
+    TEXT = 'text/plain'
+    XML = 'application/xml'
+    JPG = 'image/jpeg'
+    PNG = 'image/png'
 
 class Mode:
     BLOWER = 'blower'
