d75e41cb30df773c1fa4bef31fb1b845527e53b2
Jon Graf <jgraf@vermeer.com>
2026-01-01 21:09:08 -0600

    fixed garage utils functions now that we are going straight to db

--- test/unit/utilities/test_user_garage_utils.py
+++ test/unit/utilities/test_user_garage_utils.py
@@ -1,13 +1,11 @@
 from mock import patch
 
-from svc.constants.settings_state import Settings
 from svc.utilities.user_garage_utils import get_garage_url_by_user
 
 
 @patch('svc.utilities.user_garage_utils.UserDatabaseManager')
 def test_get_garage_url_by_user__should_call_database_when_settings_cannot_be_found(mock_db):
     user_id = 'heyImAUserId'
-    Settings.get_instance(True, {})
     get_garage_url_by_user(user_id)
 
     mock_db.return_value.__enter__.return_value.get_user_garage_ip.assert_called_with(user_id)
@@ -15,7 +13,6 @@ def test_get_garage_url_by_user__should_call_database_when_settings_cannot_be_fo
 @patch('svc.utilities.user_garage_utils.UserDatabaseManager')
 def test_get_garage_url_by_user__should_return_url_with_ip_from_database(mock_db):
     user_id = 'heyImAUserId'
-    Settings.get_instance(True, {})
     database_response = '1.1.1.1'
     mock_db.return_value.__enter__.return_value.get_user_garage_ip.return_value = database_response
     actual = get_garage_url_by_user(user_id)
