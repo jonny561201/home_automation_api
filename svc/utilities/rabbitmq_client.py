b33337d427fb42e737c783cdb84997fa8aa113c1
Jon Graf <jgraf@vermeer.com>
2026-01-17 13:30:21 -0600

    attempting to simplify the publish for the moment as I dont think the primary api will ever be a consumer

--- svc/utilities/rabbitmq_client.py
+++ svc/utilities/rabbitmq_client.py
@@ -3,80 +3,110 @@ from functools import partial
 
 import pika
 
-from svc.config.settings_state import Settings
+from svc.config.settings_state import Settings, Queue
 from utilities.event_client import MyThread
 
 
-class RabbitMQClient:
+def publish(queue_name, payload):
+    settings = Settings.get_instance().Queue
 
-    def __init__(self, settings: Settings):
-        self.settings = settings.Queue
-        self.queue_name = 'my_queue'
-        self.exchange = 'home_automation'
-        self._connection = None
-        self._channel = None
+    try:
+        connection = _open_connection(settings)
+    except Exception as exc:
+        raise Exception(f'Broker Unavailable: \n {str(exc)}')
+    try:
+        channel = connection.channel()
+        channel.exchange_declare(exchange=settings.exchange, exchange_type='direct', durable=False)
 
-    def publish(self, routing_key: str, payload: dict, persistent: bool = False):
+        channel.queue_declare(queue=queue_name, durable=True)
+        channel.queue_bind(queue=queue_name, exchange=settings.exchange, routing_key=queue_name)
+
+        body = json.dumps(payload).encode('utf-8')
+        props = pika.BasicProperties(content_type='application/json', delivery_mode=1)
+        channel.basic_publish(exchange=settings.exchange, routing_key=queue_name, body=body, properties=props)
+    finally:
         try:
-            connection = self._open_connection()
-        except Exception as exc:
-            raise Exception(f'Broker Unavailable: \n {str(exc)}')
-        try:
-            channel = connection.channel()
-            channel.exchange_declare(exchange=self.exchange, exchange_type='direct', durable=False)
-            body = json.dumps(payload).encode('utf-8')
-            props = pika.BasicProperties(content_type='application/json', delivery_mode=2 if persistent else 1)
-            channel.basic_publish(exchange=self.exchange, routing_key=routing_key, body=body, properties=props)
-        finally:
-            self._close(connection)
-
-    def start_consumer(self, worker_function):
-        t = MyThread.get_instance()
-        bound_worker = partial(self._consume, worker_function)
-        t.initialize(bound_worker)
-        t.start()
-
-    def stop_consumer(self):
-        if not self._connection or not self._channel:
-            return
-
-        try:
-            self._connection.add_callback_threadsafe(lambda: self._channel.stop_consuming())
-        except Exception:
-            try:
-                self._connection.add_callback_threadsafe(lambda: self._connection.close())
-            except Exception:
-                pass
-
-        t = MyThread.get_instance()
-        t.stop()
-        try:
-            t.join(5)
-        except RuntimeError:
-            pass
-
-    def _open_connection(self):
-        credentials = pika.PlainCredentials(self.settings.user_name, self.settings.password)
-        params = pika.ConnectionParameters(host=self.settings.host, port=self.settings.port,
-                                           virtual_host=self.settings.vhost, credentials=credentials, socket_timeout=2)
-        return pika.BlockingConnection(params)
-
-    def _consume(self, worker_function):
-        try:
-            self._connection = self._open_connection()
-        except Exception as exc:
-            raise Exception(f'Broker Unavailable: \n {str(exc)}')
-        try:
-            self._channel = self._connection.channel()
-            self._channel.basic_consume(queue=self.queue_name, on_message_callback=worker_function, auto_ack=False)
-            self._channel.start_consuming()
-        finally:
-            self._close()
-            self._connection = None
-            self._channel = None
-
-    def _close(self, connection=None):
-        try:
-            connection.close() if connection else self._connection.close()
+            connection.close()
         except Exception:
             pass
+
+def _open_connection(settings: Queue):
+    credentials = pika.PlainCredentials(settings.user_name, settings.password)
+    params = pika.ConnectionParameters(host=settings.host, port=settings.port, virtual_host=settings.vhost, credentials=credentials, socket_timeout=2)
+
+    return pika.BlockingConnection(params)
+
+
+# class RabbitMQClient:
+#
+#     def __init__(self, settings: Settings):
+#         self.settings = settings.Queue
+#         self.queue_name = 'my_queue'
+#         self.exchange = 'home_automation'
+#         self._connection = None
+#         self._channel = None
+#
+#     def publish(self, routing_key: str, payload: dict, persistent: bool = False):
+#         try:
+#             connection = self._open_connection()
+#         except Exception as exc:
+#             raise Exception(f'Broker Unavailable: \n {str(exc)}')
+#         try:
+#             channel = connection.channel()
+#             channel.exchange_declare(exchange=self.exchange, exchange_type='direct', durable=False)
+#             body = json.dumps(payload).encode('utf-8')
+#             props = pika.BasicProperties(content_type='application/json', delivery_mode=2 if persistent else 1)
+#             channel.basic_publish(exchange=self.exchange, routing_key=routing_key, body=body, properties=props)
+#         finally:
+#             self._close(connection)
+#
+#     def start_consumer(self, worker_function):
+#         t = MyThread.get_instance()
+#         bound_worker = partial(self._consume, worker_function)
+#         t.initialize(bound_worker)
+#         t.start()
+#
+#     def stop_consumer(self):
+#         if not self._connection or not self._channel:
+#             return
+#
+#         try:
+#             self._connection.add_callback_threadsafe(lambda: self._channel.stop_consuming())
+#         except Exception:
+#             try:
+#                 self._connection.add_callback_threadsafe(lambda: self._connection.close())
+#             except Exception:
+#                 pass
+#
+#         t = MyThread.get_instance()
+#         t.stop()
+#         try:
+#             t.join(5)
+#         except RuntimeError:
+#             pass
+#
+#     def _open_connection(self):
+#         credentials = pika.PlainCredentials(self.settings.user_name, self.settings.password)
+#         params = pika.ConnectionParameters(host=self.settings.host, port=self.settings.port,
+#                                            virtual_host=self.settings.vhost, credentials=credentials, socket_timeout=2)
+#         return pika.BlockingConnection(params)
+#
+#     def _consume(self, worker_function):
+#         try:
+#             self._connection = self._open_connection()
+#         except Exception as exc:
+#             raise Exception(f'Broker Unavailable: \n {str(exc)}')
+#         try:
+#             self._channel = self._connection.channel()
+#             self._channel.basic_consume(queue=self.queue_name, on_message_callback=worker_function, auto_ack=False)
+#             self._channel.start_consuming()
+#         finally:
+#             self._close()
+#             self._connection = None
+#             self._channel = None
+#
+#     def _close(self, connection=None):
+#         try:
+#             connection.close() if connection else self._connection.close()
+#         except Exception:
+#             pass
