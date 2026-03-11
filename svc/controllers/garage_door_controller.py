f38abbff2a3f7c917676ad1cc3149a9c847298fc
Jon Graf <jgraf@vermeer.com>
2026-03-11 12:47:59 -0500

    updated the garage toggle and state change functions to only call the queue...working end to end now

--- svc/controllers/garage_door_controller.py
+++ svc/controllers/garage_door_controller.py
@@ -1,21 +1,28 @@
+from svc.models.garage import GarageState
+from svc.constants.home_automation import Automation
 from svc.utilities import api_utils
 from svc.utilities.jwt_utils import is_jwt_valid
 from svc.utilities.user_garage_utils import get_garage_url_by_user
+from svc.utilities.rabbitmq_client import publish
 
 
 def get_status(bearer_token, user_id, garage_id):
     is_jwt_valid(bearer_token)
     base_url = get_garage_url_by_user(user_id)
+
     return api_utils.get_garage_door_status(bearer_token, base_url, garage_id)
 
 
 def update_state(bearer_token, user_id, garage_id, request):
     is_jwt_valid(bearer_token)
-    base_url = get_garage_url_by_user(user_id)
-    return api_utils.update_garage_door_state(bearer_token, base_url, garage_id, request)
+    message = {'id': garage_id, 'action': 'update', 'open': request['garageDoorOpen']}
+    publish(Automation.GARAGE.QUEUE, message)
+
+    return GarageState(isGarageOpen=request['garageDoorOpen'])
 
 
 def toggle_door(bearer_token, user_id, garage_id):
     is_jwt_valid(bearer_token)
-    base_url = get_garage_url_by_user(user_id)
-    api_utils.toggle_garage_door_state(bearer_token, base_url, garage_id)
+    message = {'id': garage_id, 'action': 'toggle'}
+
+    publish(Automation.GARAGE.QUEUE, message)
