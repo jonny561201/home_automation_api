cb7b82a0afbe986b4bce09651ed3ad03af81c78a
Jon Graf <jgraf@vermeer.com>
2026-03-13 11:00:25 -0500

    updated the weather request and temperature services to map the new response to the same output

--- svc/services/temperature.py
+++ svc/services/temperature.py
@@ -1,13 +1,10 @@
-from svc.config.settings_state import Settings
 from svc.services.weather_request import get_weather
 from svc.utilities.file_utils import read_temperature_file
 from svc.utilities.user_temp_utils import get_user_temperature
 
 
 def get_external_temp(preference):
-    temp_unit = "metric" if preference.tempUnit == "celsius" else "imperial"
-    settings = Settings.get_instance()
-    return get_weather(preference.city, temp_unit, settings.weather_app_id)
+    return get_weather(preference.city, preference.tempUnit)
 
 
 def get_internal_temp(preference):
--- svc/services/weather_request.py
+++ svc/services/weather_request.py
@@ -2,27 +2,29 @@ import logging
 
 from requests.exceptions import ConnectionError
 
+from svc.constants.weather import WEATHER
 from svc.models.thermostat import DailyForecast
-from svc.utilities.api_utils import get_weather_by_city, get_forecast_by_coords
+from svc.utilities.api_utils import get_city_coordinates, get_forecast_by_coords
 
 
-def get_weather(city, unit, app_id):
-    weather = {}
+def get_weather(city, unit):
     try:
-        weather = get_weather_by_city(city, unit, app_id)
-        forecast = get_forecast_by_coords(weather['coord'], unit, app_id)
-        daily_forecast = forecast['daily'][0]['temp']
-        return __build_response(weather, daily_forecast)
+        city = get_city_coordinates(city)
+        coords = city.get('results', [])[0]
+        forecast = get_forecast_by_coords(coords['latitude'], coords['longitude'], unit)
+        return __build_response(forecast)
     except (ConnectionError, KeyError, IndexError):
         logging.info('Weather API connection error!')
-        return __build_response(weather, {})
+        return __build_response({})
 
 
-def __build_response(weather, daily_forecast):
-    main = weather.get('main', {})
-    current_temp = main.get('temp', 0.0)
-    min_temp = daily_forecast.get('min', 0.0)
-    max_temp = daily_forecast.get('max', 0.0)
-    description = next(iter(weather.get('weather', {})), {}).get('description', '')
+def __build_response(daily_forecast):
+    daily = daily_forecast.get('daily', {})
+    current = daily_forecast.get('current_weather', {})
+    current_temp = current.get('temperature', 0.0)
+    code = current.get('weathercode', 0)
+    description = WEATHER.get(code)
+    min_temp = daily.get('temperature_2m_min', [0.0])[0]
+    max_temp = daily.get('temperature_2m_max', [0.0])[0]
 
     return DailyForecast(temp=current_temp, minTemp=min_temp, maxTemp=max_temp, description=description)
