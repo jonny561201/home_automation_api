1882b4b8746146c3ee9688204db0a0bdf5490385
Jon Graf <jgraf@vermeer.com>
2026-03-13 12:05:35 -0500

    cleaned up and simplified the unit tests for the weather service

--- test/unit/services/test_weather_request.py
+++ test/unit/services/test_weather_request.py
@@ -5,128 +5,113 @@ from svc.services.weather_request import get_weather
 
 
 @patch('svc.services.weather_request.get_forecast_by_coords')
-@patch('svc.services.weather_request.get_weather_by_city')
+@patch('svc.services.weather_request.get_city_coordinates')
 class TestWeatherRequest:
     CITY = 'Prague'
     UNIT = 'metric'
-    APP_ID = 'abc123'
-    STATUS_OK = 200
-    STATUS_BAD = 400
-    STATUS_UNAUTHORIZED = 401
-    COORDS = {'lat': 92.00, 'lon': -93.85}
-    DAY = {'temp': {'min': 12.34, 'max': 12.87}}
+    COORDS = {'latitude': 92.00, 'longitude': -93.85}
 
     def setup_method(self):
-        self.WEATHER_RESPONSE = {'coord': self.COORDS, 'main': {}, 'weather': [{}]}
-        self.FORECAST_RESPONSE = {'daily': []}
+        self.DAY = {'temperature_2m_min': [12.34], 'temperature_2m_max': [12.87]}
+        self.TEMP = {'temperature': 64.8, 'weathercode': 2}
+        self.CITY_RESPONSE = {'results': [self.COORDS]}
+        self.FORECAST_RESPONSE = {'daily': self.DAY, 'current_weather': self.TEMP}
 
-    def test_get_weather__should_return_temp_data(self, mock_weather, mock_forecast):
-        expected_temp = 64.8
-        self.WEATHER_RESPONSE['main']['temp'] = expected_temp
-        mock_weather.return_value = self.WEATHER_RESPONSE
+    def test_get_weather__should_return_temp_data(self, mock_city, mock_forecast):
+        mock_city.return_value = self.CITY_RESPONSE
+        mock_forecast.return_value = self.FORECAST_RESPONSE
 
-        actual = get_weather(self.CITY, self.UNIT, self.APP_ID)
+        actual = get_weather(self.CITY, self.UNIT)
 
-        assert actual.temp == expected_temp
+        assert actual.temp == self.TEMP['temperature']
 
-    def test_get_weather__should_return_default_temp_value_of_zero(self, mock_weather, mock_forecast):
-        mock_weather.return_value = self.WEATHER_RESPONSE
+    def test_get_weather__should_return_default_temp_value_of_zero(self, mock_city, mock_forecast):
+        self.TEMP = {'weathercode': 0}
+        self.FORECAST_RESPONSE = {'daily': self.DAY, 'current_weather': self.TEMP}
+        mock_city.return_value = self.CITY_RESPONSE
+        mock_forecast.return_value = self.FORECAST_RESPONSE
 
-        actual = get_weather(self.CITY, self.UNIT, self.APP_ID)
+        actual = get_weather(self.CITY, self.UNIT)
 
         assert actual.temp == 0.0
 
-    def test_get_weather__should_return_min_temp_value(self, mock_weather, mock_forecast):
-        self.FORECAST_RESPONSE['daily'].append(self.DAY)
+    def test_get_weather__should_return_min_temp_value(self, mock_city, mock_forecast):
         mock_forecast.return_value = self.FORECAST_RESPONSE
-        mock_weather.return_value = self.WEATHER_RESPONSE
+        mock_city.return_value = self.CITY_RESPONSE
 
-        actual = get_weather(self.CITY, self.UNIT, self.APP_ID)
+        actual = get_weather(self.CITY, self.UNIT)
 
-        assert actual.minTemp == self.DAY['temp']['min']
+        assert actual.minTemp == self.DAY['temperature_2m_min'][0]
 
-    def test_get_weather__should_return_default_min_temp_value(self, mock_weather, mock_forecast):
-        mock_weather.return_value = self.WEATHER_RESPONSE
+    def test_get_weather__should_return_default_min_temp_value(self, mock_city, mock_forecast):
+        self.FORECAST_RESPONSE = {'daily': {}, 'current_weather': self.TEMP}
+        mock_city.return_value = self.CITY_RESPONSE
         mock_forecast.return_value = self.FORECAST_RESPONSE
 
-        actual = get_weather(self.CITY, self.UNIT, self.APP_ID)
+        actual = get_weather(self.CITY, self.UNIT)
 
         assert actual.minTemp == 0.0
 
-    def test_get_weather__should_return_max_temp_value(self, mock_weather, mock_forecast):
-        self.FORECAST_RESPONSE['daily'].append(self.DAY)
-        mock_weather.return_value = self.WEATHER_RESPONSE
+    def test_get_weather__should_return_max_temp_value(self, mock_city, mock_forecast):
+        mock_city.return_value = self.CITY_RESPONSE
         mock_forecast.return_value = self.FORECAST_RESPONSE
 
-        actual = get_weather(self.CITY, self.UNIT, self.APP_ID)
+        actual = get_weather(self.CITY, self.UNIT)
 
-        assert actual.maxTemp == self.DAY['temp']['max']
+        assert actual.maxTemp == self.DAY['temperature_2m_max'][0]
 
-    def test_get_weather__should_return_default_max_temp_value(self, mock_weather, mock_forecast):
-        mock_weather.return_value = self.WEATHER_RESPONSE
+    def test_get_weather__should_return_default_max_temp_value(self, mock_city, mock_forecast):
+        self.FORECAST_RESPONSE = {'daily': {}, 'current_weather': self.TEMP}
+        mock_city.return_value = self.CITY_RESPONSE
         mock_forecast.return_value = self.FORECAST_RESPONSE
 
-        actual = get_weather(self.CITY, self.UNIT, self.APP_ID)
+        actual = get_weather(self.CITY, self.UNIT)
 
         assert actual.maxTemp == 0.0
 
-    def test_get_weather__should_return_weather_description(self, mock_weather, mock_forecast):
-        forecast_description = 'fake forecast'
-        self.WEATHER_RESPONSE['weather'][0]['description'] = forecast_description
-        mock_weather.return_value = self.WEATHER_RESPONSE
-
-        actual = get_weather(self.CITY, self.UNIT, self.APP_ID)
-
-        assert actual.description == forecast_description
-
-    def test_get_weather__should_return_default_weather_description(self, mock_weather, mock_forecast):
-        mock_weather.return_value = self.WEATHER_RESPONSE
-
-        actual = get_weather(self.CITY, self.UNIT, self.APP_ID)
-
-        assert actual.description == ""
-
-    def test_get_weather__should_return_default_values_when_not_ok_status_returned(self, mock_weather, mock_forecast):
-        mock_weather.return_value = {}
+    def test_get_weather__should_return_weather_description(self, mock_city, mock_forecast):
+        mock_city.return_value = self.CITY_RESPONSE
         mock_forecast.return_value = self.FORECAST_RESPONSE
 
-        actual = get_weather(self.CITY, self.UNIT, self.APP_ID)
+        actual = get_weather(self.CITY, self.UNIT)
 
-        assert actual.temp == 0.0
-        assert actual.minTemp == 0.0
-        assert actual.maxTemp == 0.0
-        assert actual.description == ""
+        assert actual.description == 'partly cloudy'
 
-    def test_get_weather__should_return_weather_values_and_default_forecast(self, mock_weather, mock_forecast):
-        temp = 73.23
-        description = 'fake forecast'
-        mock_forecast.return_value = {}
-        mock_weather.return_value = self.WEATHER_RESPONSE
-        self.WEATHER_RESPONSE['main']['temp'] = temp
-        self.WEATHER_RESPONSE['weather'][0]['description'] = description
+    def test_get_weather__should_return_default_weather_description(self, mock_city, mock_forecast):
+        self.FORECAST_RESPONSE = {'daily': self.DAY, 'current_weather': {}}
+        mock_city.return_value = self.CITY_RESPONSE
+        mock_forecast.return_value = self.FORECAST_RESPONSE
 
-        actual = get_weather(self.CITY, self.UNIT, self.APP_ID)
+        actual = get_weather(self.CITY, self.UNIT)
 
-        assert actual.temp == temp
-        assert actual.minTemp == 0.0
-        assert actual.maxTemp == 0.0
-        assert actual.description == description
+        assert actual.description == 'sunny'
 
-    def test_get_weather__should_return_default_values_when_throws_connection_error(self, mock_weather, mock_forecast):
-        mock_weather.side_effect = ConnectionError()
+    def test_get_weather__should_return_default_values_when_not_ok_status_returned(self, mock_city, mock_forecast):
+        mock_city.return_value = {}
+        mock_forecast.return_value = self.FORECAST_RESPONSE
 
-        actual = get_weather(self.CITY, self.UNIT, self.APP_ID)
+        actual = get_weather(self.CITY, self.UNIT)
 
         assert actual.temp == 0.0
         assert actual.minTemp == 0.0
         assert actual.maxTemp == 0.0
-        assert actual.description == ""
+        assert actual.description == 'sunny'
+
+    def test_get_weather__should_return_default_values_when_throws_connection_error(self, mock_city, mock_forecast):
+        mock_city.side_effect = ConnectionError()
+
+        actual = get_weather(self.CITY, self.UNIT)
+
+        assert actual.temp == 0.0
+        assert actual.minTemp == 0.0
+        assert actual.maxTemp == 0.0
+        assert actual.description == 'sunny'
         
-    def test_get_weather__should_make_call_to_get_forecast_data(self, mock_weather, mock_forecast):
-        get_weather(self.CITY, self.UNIT, self.APP_ID)
-        mock_forecast.assert_called_with(ANY, self.UNIT, self.APP_ID)
+    def test_get_weather__should_make_call_to_get_city_coords(self, mock_city, mock_forecast):
+        get_weather(self.CITY, self.UNIT)
+        mock_city.assert_called_with(self.CITY)
 
-    def test_get_weather__should_call_forecast_using_coords_from_weather_call(self, mock_weather, mock_forecast):
-        mock_weather.return_value = self.WEATHER_RESPONSE
-        get_weather(self.CITY, self.UNIT, self.APP_ID)
-        mock_forecast.assert_called_with(self.COORDS, ANY, ANY)
+    def test_get_weather__should_call_forecast_using_coords_from_weather_call(self, mock_city, mock_forecast):
+        mock_city.return_value = self.CITY_RESPONSE
+        get_weather(self.CITY, self.UNIT)
+        mock_forecast.assert_called_with(self.COORDS['latitude'], self.COORDS['longitude'], self.UNIT)
