b352537395658d43c6719e2eb06620cef7675501
Jon Graf <jgraf@vermeer.com>
2026-03-17 10:48:30 -0500

    fixed issue with import

--- svc/db/repositories/lights_repository.py
+++ svc/db/repositories/lights_repository.py
@@ -2,7 +2,7 @@ from sqlalchemy import select, delete
 
 from svc.db.repositories.database_base import DatabaseBase
 from svc.db.models.user_information_model import Scenes, SceneDetails
-from models.scenes import LightScenes, LightScene, LightDetail
+from svc.models.scenes import LightScenes, LightScene, LightDetail
 
 
 class LightsRepository(DatabaseBase):
