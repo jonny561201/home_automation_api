ad721b32332f88cec8991bef5d490d06fe3bad21
Jon Graf <jgraf@vermeer.com>
2026-03-20 13:31:59 -0500

    updated the make the is_jwt_valid contract closer to the new auth client

--- svc/utilities/jwt_utils.py
+++ svc/utilities/jwt_utils.py
@@ -12,7 +12,12 @@ from svc.config.settings_state import Settings
 def is_jwt_valid(jwt_token):
     if jwt_token is None:
         raise Unauthorized
-    _parse_jwt_token(jwt_token)
+    try:
+        stripped_token = jwt_token.replace('Bearer ', '')
+        settings = Settings.get_instance()
+        return jwt.decode(stripped_token, settings.jwt_secret, algorithms=["HS256"])
+    except (InvalidSignatureError, ExpiredSignatureError, DecodeError, KeyError) as er:
+        raise Unauthorized
 
 
 def create_jwt_token(user_info, refresh_token):
@@ -26,14 +31,6 @@ def create_jwt_token(user_info, refresh_token):
 def generate_refresh_token():
     return str(uuid.uuid4())
 
-
-def _parse_jwt_token(jwt_token):
-    try:
-        stripped_token = jwt_token.replace('Bearer ', '')
-        settings = Settings.get_instance()
-        jwt.decode(stripped_token, settings.jwt_secret, algorithms=["HS256"])
-    except (InvalidSignatureError, ExpiredSignatureError, DecodeError, KeyError) as er:
-        raise Unauthorized
 
 
 
@@ -49,12 +46,10 @@ class AuthClient:
     def verify_jwt(self, token: str):
         signing_key = self.jwks_client.get_signing_key_from_jwt(token)
 
-        payload = jwt.decode(
+        return jwt.decode(
             token,
             signing_key.key,
             algorithms=self.ALGORITHMS,
             audience=self.settings.Authority.audience,
             issuer=f"https://{self.settings.Authority.domain}/",
         )
-
-        return payload
