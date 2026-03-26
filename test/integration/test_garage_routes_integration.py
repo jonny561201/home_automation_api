abb2b1cd919f5b957733c826d3441ffdd48ed401
Jon Graf <jgraf@vermeer.com>
2026-03-26 15:21:11 -0500

    fixed the garage door routes integration tests by adding user id to jwt and content type for posts

--- test/integration/test_garage_routes_integration.py
+++ test/integration/test_garage_routes_integration.py
@@ -21,6 +21,8 @@ class TestGarageDoorRoutesIntegration:
     ROLE_ID = str(uuid.uuid4())
     USER_ROLE_ID = str(uuid.uuid4())
     DEVICE_ID = str(uuid.uuid4())
+    BEARER_TOKEN = jwt.encode({'sub': USER_ID}, JWT_SECRET, algorithm='HS256')
+    HEADERS = {'Authorization': BEARER_TOKEN, 'Content-Type': 'application/json'}
 
     def setup_method(self):
         Settings.get_instance()._settings = {'JwtSecret': self.JWT_SECRET}
@@ -46,8 +48,7 @@ class TestGarageDoorRoutesIntegration:
             database.session.execute(delete(UserInformation).where(UserInformation.id == self.USER_ID))
 
     def test_get_garage_door_status__should_return_unauthorized_with_no_header(self, mock_request):
-        url = f'garageDoor/{self.GARAGE_ID}/user/{self.USER_ID}/status'
-        actual = self.TEST_CLIENT.get(url)
+        actual = self.TEST_CLIENT.get(f'garageDoor/{self.GARAGE_ID}/status')
 
         assert actual.status_code == 401
 
@@ -56,19 +57,14 @@ class TestGarageDoorRoutesIntegration:
         response._content = json.dumps({'isGarageOpen': False, 'statusDuration': datetime.now().isoformat(), 'coordinates': {'latitude': 1.12, 'longitude': -12.93}}).encode()
         response.status_code = 200
         mock_request.get.return_value = response
-        bearer_token = jwt.encode({}, self.JWT_SECRET, algorithm='HS256')
-        headers = {'Authorization': bearer_token}
-        url = f'garageDoor/{self.GARAGE_ID}/user/{self.USER_ID}/status'
-        actual = self.TEST_CLIENT.get(url, headers=headers)
+        actual = self.TEST_CLIENT.get(f'garageDoor/{self.GARAGE_ID}/status', headers=self.HEADERS)
 
         assert actual.status_code == 200
 
     def test_update_garage_door_state__should_return_unauthorized_without_jwt(self, mock_request):
-        post_body = "{}"
         headers = {'Content-Type': 'application/json'}
 
-        url = f'garageDoor/{self.GARAGE_ID}/state'
-        actual = self.TEST_CLIENT.post(url, data=post_body, headers=headers)
+        actual = self.TEST_CLIENT.post(f'garageDoor/{self.GARAGE_ID}/state', data='{}', headers=headers)
 
         assert actual.status_code == 401
 
@@ -84,23 +80,17 @@ class TestGarageDoorRoutesIntegration:
 
     def test_update_garage_door_state__should_return_bad_request_when_malformed_json(self, mock_request):
         post_body = {'badKey': 'fakerequest'}
-        bearer_token = jwt.encode({}, self.JWT_SECRET, algorithm='HS256')
-        headers = {'Authorization': bearer_token, 'Content-Type': 'application/json'}
 
-        url = f'garageDoor/{self.GARAGE_ID}/state'
-        actual = self.TEST_CLIENT.post(url, data=json.dumps(post_body), headers=headers)
+        actual = self.TEST_CLIENT.post(f'garageDoor/{self.GARAGE_ID}/state', data=json.dumps(post_body), headers=self.HEADERS)
 
         assert actual.status_code == 400
 
     def test_toggle_garage_door__should_return_success(self, mock_request):
-        bearer_token = jwt.encode({}, self.JWT_SECRET, algorithm='HS256')
-        headers = {'Authorization': bearer_token}
         response = Response()
         response.status_code = 200
         mock_request.get.return_value = response
 
-        url = f'garageDoor/{self.GARAGE_ID}/toggle'
-        actual = self.TEST_CLIENT.get(url, headers=headers)
+        actual = self.TEST_CLIENT.get(f'garageDoor/{self.GARAGE_ID}/toggle', headers=self.HEADERS)
 
         assert actual.status_code == 200
 
@@ -108,7 +98,6 @@ class TestGarageDoorRoutesIntegration:
         bearer_token = jwt.encode({}, 'bad_secret', algorithm='HS256')
         headers = {'Authorization': bearer_token}
 
-        url = f'garageDoor/{self.GARAGE_ID}/toggle'
-        actual = self.TEST_CLIENT.get(url, headers=headers)
+        actual = self.TEST_CLIENT.get(f'garageDoor/{self.GARAGE_ID}/toggle', headers=headers)
 
         assert actual.status_code == 401
