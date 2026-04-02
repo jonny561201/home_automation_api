28cf193b2a91c66a6a3c6fd5e1b1872b487ce0bd
Jon Graf <jgraf@vermeer.com>
2026-04-02 15:31:59 -0500

    created migrations to drop irrelevant tables around update the devices table to consolidate concepts and simplify

---
+++ docker/flyway/migration/V1.28__add_device_types_table.sql
@@ -0,0 +1,26 @@
+create table device_type (
+    ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    type VARCHAR(255) NOT NULL
+);
+
+INSERT INTO device_type (type) VALUES
+   ('Garage Door'),
+   ('Thermostat'),
+   ('Security System'),
+   ('Lighting'),
+   ('Sump Pump');
+
+DROP TABLE role_device_nodes;
+DROP TABLE role_devices;
+DROP TABLE user_roles;
+DROP TABLE roles;
+
+CREATE TABLE devices (
+    ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES user_information(ID),
+    device_type_id UUID NOT NULL REFERENCES device_type(ID),
+    ip_address INET NOT NULL,
+    ip_port INTEGER CHECK (ip_port BETWEEN 1 AND 65535),
+    node_name VARCHAR(50) NOT NULL,
+    node_device SMALLINT NOT NULL
+);
---
+++ docker/flyway/migration/V1.29__remove_token_tables.sql
@@ -0,0 +1,2 @@
+DROP TABLE refresh_token;
+DROP TABLE user_login;
