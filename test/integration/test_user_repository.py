98af735f25d4d3a45b44124dd62dffd10bbd4239
Jon Graf <jgraf@vermeer.com>
2026-04-04 20:25:32 -0500

    cleaned up some more of the auth database to user repository

--- test/integration/test_user_repository.py
+++ test/integration/test_user_repository.py
@@ -3,7 +3,7 @@ import uuid
 
 import pytest
 from sqlalchemy import delete, select
-from werkzeug.exceptions import NotFound
+from werkzeug.exceptions import NotFound, Unauthorized
 
 from svc.db.models.user_information_model import UserInformation, ScheduleTasks, UserPreference
 from svc.db.repositories.database_base import DatabaseBase
@@ -151,3 +151,32 @@ class TestUserIntegration:
             assert actual.is_imperial is True
             assert actual.garage_door == self.GARAGE
             assert actual.garage_id is None
+
+
+class TestDbCredentialIntegration:
+    FIRST = 'Jon'
+    LAST = 'Test'
+    USER_ID = str(uuid.uuid4())
+
+    def setup_method(self):
+        self.USER = UserInformation(id=self.USER_ID, first_name=self.FIRST, last_name=self.LAST)
+        with DatabaseBase() as database:
+            database.session.add(self.USER)
+
+    def teardown_method(self):
+        with DatabaseBase() as database:
+            database.session.execute(delete(UserInformation).where(UserInformation.id == self.USER_ID))
+
+    def test_get_user_info__should_return_user_information(self):
+        with UserRepository() as database:
+            actual = database.get_user_info(self.USER_ID)
+
+            assert actual['user_id'] == self.USER_ID
+            assert actual['first_name'] == self.FIRST
+            assert actual['last_name'] == self.LAST
+
+    def test_get_user_info__should_raise_unauthorized_when_user_not_found(self):
+        with pytest.raises(Unauthorized):
+            with UserRepository() as database:
+                missing_user_id = str(uuid.uuid4())
+                database.get_user_info(missing_user_id)
