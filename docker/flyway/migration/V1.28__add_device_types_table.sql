cbe2af1745444b22f4742cd2ba4ea8b2b6b15644
Jon Graf <jgraf@vermeer.com>
2026-04-09 14:24:48 -0500

    updated migration to allow user id to be nullable for devices

--- docker/flyway/migration/V1.28__add_device_types_table.sql
+++ docker/flyway/migration/V1.28__add_device_types_table.sql
@@ -18,7 +18,7 @@ DROP TABLE roles;
 
 CREATE TABLE devices (
     ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID NOT NULL REFERENCES user_information(ID),
+    user_id UUID REFERENCES user_information(ID),
     device_type_id UUID NOT NULL REFERENCES device_type(ID),
     ip_address INET NOT NULL,
     ip_port INTEGER CHECK (ip_port BETWEEN 1 AND 65535),
