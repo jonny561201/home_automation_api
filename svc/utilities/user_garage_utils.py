1b99b8f82ab0b98a3c2a01dc63c2a714b38ccceb
Jon Graf <jgraf@vermeer.com>
2026-04-13 16:41:54 -0500

    removed now unnecessary multicast test, unnecessary test data migration, and some other cleanup

--- svc/utilities/user_garage_utils.py
+++
@@ -1,7 +0,0 @@
-from svc.db.repositories.device_repository import DeviceRepository
-
-
-def get_garage_url_by_user(user_id):
-    with DeviceRepository() as database:
-        ip = database.get_user_garage_ip(user_id)
-        return f'http://{ip}'
