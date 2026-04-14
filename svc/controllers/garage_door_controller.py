8f66ca5264c4f94094a8b5bd5d53f8ab65d6e6d1
Jon Graf <jgraf@vermeer.com>
2026-04-14 08:53:13 -0500

    garage controller is now filtering the garage doors that arent named out of the list

--- svc/controllers/garage_door_controller.py
+++ svc/controllers/garage_door_controller.py
@@ -9,29 +9,28 @@ from svc.utilities.rabbitmq_client import publish
 
 
 def get_status(bearer_token, garage_id):
-    claims = AuthClient.get_instance().verify_jwt(bearer_token)
-    user_id = claims[AuthClaims.USER_ID]
-    with DeviceRepository() as db:
-        info = db.get_device_address_info(user_id)
-        if not info:
-            raise FailedDependency(description="device not registered")
-        url = f'http://{info.ip_address}:{info.ip_port}'
+    info = _get_garage_device_info(bearer_token)
+    url = f'http://{info.ip_address}:{info.ip_port}'
+    door_name = info.node_names.get(int(garage_id))
+    if not door_name:
+        raise BadRequest(description="garage door not registered")
     status = api_utils.get_garage_door_status(info.api_key, url, garage_id)
-    status['doorName'] = info.node_names.get(int(garage_id))
+    status['doorName'] = door_name
     return GarageStatus.from_dict(status)
 
 
 def get_all_status(bearer_token):
-    claims = AuthClient.get_instance().verify_jwt(bearer_token)
-    user_id = claims[AuthClaims.USER_ID]
-    with DeviceRepository() as db:
-        info = db.get_device_address_info(user_id)
-        if not info:
-            raise FailedDependency(description="device not registered")
-        url = f'http://{info.ip_address}:{info.ip_port}'
+    info = _get_garage_device_info(bearer_token)
+    url = f'http://{info.ip_address}:{info.ip_port}'
     status = api_utils.get_all_garage_doors_status(info.api_key, url)
+
+    registered_doors = []
     for door in status['doors']:
-        door['doorName'] = info.node_names.get(int(door['garageId']))
+        door_name = info.node_names.get(int(door['garageId']))
+        if door_name:
+            door['doorName'] = door_name
+            registered_doors.append(door)
+    status['doors'] = registered_doors
     return GarageOverview.from_dict(status)
 
 
@@ -51,3 +50,13 @@ def toggle_door(bearer_token, garage_id):
     message = {'id': garage_id, 'action': 'toggle'}
 
     publish(Automation.GARAGE.QUEUE, message)
+
+
+def _get_garage_device_info(bearer_token):
+    claims = AuthClient.get_instance().verify_jwt(bearer_token)
+    user_id = claims[AuthClaims.USER_ID]
+    with DeviceRepository() as db:
+        info = db.get_device_address_info(user_id)
+    if not info:
+        raise FailedDependency(description="device not registered")
+    return info
