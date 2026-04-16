7bb2f51964a0807ac68c3bc8efe205c336af3769
Jon Graf <jgraf@vermeer.com>
2026-04-16 12:08:34 -0500

    updated the settings file to blow the stack if they arent found instead of silently failing

--- svc/config/settings_state.py
+++ svc/config/settings_state.py
@@ -44,13 +44,10 @@ class Settings:
         return self._settings.get('AllowedOrigins') if self._settings is not None else []
 
     def __load_settings(self):
-        try:
-            environment = os.environ.get('PYTHON_ENVIRONMENT', 'local')
-            file_path = os.path.join(os.path.dirname(__file__), '..', '..', f'settings.{environment}.json')
-            with open(file_path, "r") as reader:
-                self._settings = json.loads(reader.read())
-        except Exception:
-            self._settings = {}
+        environment = os.environ.get('PYTHON_ENVIRONMENT', 'local')
+        file_path = os.path.join(os.path.dirname(__file__), '..', '..', f'settings.{environment}.json')
+        with open(file_path, "r") as reader:
+            self._settings = json.loads(reader.read())
 
 
 class Database:
