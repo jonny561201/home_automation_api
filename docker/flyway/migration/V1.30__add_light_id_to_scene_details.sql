61b42973e5363b717820fdfa14b9ff3d1e36d1ad
Jon Graf <jgraf@vermeer.com>
2026-05-06 10:10:06 -0500

    updated migration to start storing the users location (lat/lon) to database

--- docker/flyway/migration/V1.30__add_light_id_to_scene_details.sql
+++ docker/flyway/migration/V1.30__add_light_id_to_scene_details.sql
@@ -1,1 +1,3 @@
 ALTER TABLE scene_details ADD COLUMN light_id VARCHAR(10);
+ALTER TABLE user_preferences ADD COLUMN latitude DECIMAL(9, 6);
+ALTER TABLE user_preferences ADD COLUMN longitude DECIMAL(9, 6);
